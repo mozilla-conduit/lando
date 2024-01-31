f7e93aaeccb2cc2e9c1239c986f16588bdf9c2a5
Zeid Zabaneh <2043828+zzzeid@users.noreply.github.com>
2024-01-31 15:23:21 -0500

    main: update models and migration files (bug 1869032) (#17)
    - translate remaining models to Django models
    - generate new migration file
    - port various methods and properties to use new models
    - store patch as string rather than bytes

--- src/lando/main/models/__init__.py
+++ src/lando/main/models/__init__.py
@@ -1,1 +1,3 @@
 from lando.main.models.base import *
+from lando.main.models.landing_job import *
+from lando.main.models.revision import *
--- src/lando/main/models/landing_job.py
+++ src/lando/main/models/landing_job.py
@@ -1,6 +1,5 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+from __future__ import annotations
+
 import datetime
 import enum
 import logging
@@ -12,49 +11,27 @@ from typing import (
     Optional,
 )
 
-import flask_sqlalchemy
+from django.db import models
+from django.db.models import QuerySet
+from django.utils.translation import gettext_lazy
 from mots.config import FileConfig
 from mots.directory import Directory
-from sqlalchemy import or_
-from sqlalchemy.dialects.postgresql import array
-from sqlalchemy.dialects.postgresql.json import JSONB
-from sqlalchemy.orm.attributes import flag_modified
 
-from landoapi.models.base import Base
-from landoapi.models.revisions import Revision, revision_landing_job
-from landoapi.storage import db
+from lando.main.models import BaseModel
+from lando.main.models.revision import Revision, RevisionLandingJob
 
 logger = logging.getLogger(__name__)
 
 DEFAULT_GRACE_SECONDS = int(os.environ.get("DEFAULT_GRACE_SECONDS", 60 * 2))
 
 
-@enum.unique
-class LandingJobStatus(enum.Enum):
-    """Status of a landing job.
-
-    NOTE: The definiton order is important as this enum is
-    used in a database column definition. This column is used in
-    "ORDER BY" clauses when querying the table so any change to
-    this class definition could break things. See the `status`
-    column of `LandingJob`.
-    """
-
-    # Initial creation state.
-    SUBMITTED = "SUBMITTED"
-
-    # Actively being processed.
-    IN_PROGRESS = "IN_PROGRESS"
-
-    # Temporarily failed after processing
-    DEFERRED = "DEFERRED"
-
-    # Automatic finished states.
-    FAILED = "FAILED"
-    LANDED = "LANDED"
-
-    # Manually cancelled state.
-    CANCELLED = "CANCELLED"
+class LandingJobStatus(models.TextChoices):
+    SUBMITTED = "SUBMITTED", gettext_lazy("Submitted")
+    IN_PROGRESS = "IN_PROGRESS", gettext_lazy("In progress")
+    DEFERRED = "DEFERRED", gettext_lazy("Deferred")
+    FAILED = "FAILED", gettext_lazy("Failed")
+    LANDED = "LANDED", gettext_lazy("Landed")
+    CANCELLED = "CANCELLED", gettext_lazy("Cancelled")
 
 
 @enum.unique
@@ -77,83 +54,75 @@ class LandingJobAction(enum.Enum):
     CANCEL = "CANCEL"
 
 
-class LandingJob(Base):
-    """State for a landing job."""
-
-    # The postgres enum column which this definition results in
-    # uses an enum type where the order matches the order of
-    # values at definition time. Since python `enum.Enum` is
-    # ordered, the resulting column will have the same order
-    # as the definition order of the enum. This can be relied
-    # on for comparisons or things like queries with ORDER BY.
-    status = db.Column(db.Enum(LandingJobStatus), nullable=True, default=None)
+class LandingJob(BaseModel):
+    status = models.CharField(
+        max_length=12,
+        choices=LandingJobStatus,
+        default=None,
+        null=True,  # TODO: should change this to not-nullable
+        blank=True,
+    )
 
     # revision_to_diff_id and revision_order are deprecated and kept for historical reasons.
-    revision_to_diff_id = db.Column(JSONB, nullable=True)
-    revision_order = db.Column(JSONB, nullable=True)
+    revision_to_diff_id = models.JSONField(null=True, blank=True, default=dict)
+    revision_order = models.JSONField(null=True, blank=True, default=dict)
 
     # Text describing errors when status != LANDED.
-    error = db.Column(db.Text(), default="")
+    error = models.TextField(default="", blank=True)
 
     # Error details in a dictionary format, listing failed merges, etc...
     # E.g. {
     #    "failed_paths": [{"path": "...", "url": "..."}],
-    #     "reject_paths": [{"path": "...", "content": "..."}]
+    #    "reject_paths": [{"path": "...", "content": "..."}]
     # }
-    error_breakdown = db.Column(JSONB, nullable=True)
+    error_breakdown = models.JSONField(null=True, blank=True, default=dict)
 
     # LDAP email of the user who requested transplant.
-    requester_email = db.Column(db.String(254), nullable=False)
-
-    # Lando's name for the repository.
-    repository_name = db.Column(db.Text(), nullable=False)
-
-    # URL of the repository revisions are to land to.
-    repository_url = db.Column(db.Text(), default="")
+    requester_email = models.CharField(blank=True, default="", max_length=255)
 
     # Identifier for the most descendent commit created by this landing.
-    landed_commit_id = db.Column(db.Text(), default="")
+    landed_commit_id = models.TextField(blank=True, default="")
 
     # Number of attempts made to complete the job.
-    attempts = db.Column(db.Integer, nullable=False, default=0)
+    attempts = models.IntegerField(default=0)
 
     # Priority of the job. Higher values are processed first.
-    priority = db.Column(db.Integer, nullable=False, default=0)
+    priority = models.IntegerField(default=0)
 
     # Duration of job from start to finish
-    duration_seconds = db.Column(db.Integer, default=0)
+    duration_seconds = models.IntegerField(default=0)
 
     # JSON array of changeset hashes which replaced reviewed changesets
     # after autoformatting.
     # eg.
     #    ["", ""]
-    formatted_replacements = db.Column(JSONB, nullable=True)
+    formatted_replacements = models.JSONField(null=True, blank=True, default=list)
 
     # Identifier of the published commit which this job should land on top of.
-    target_commit_hash = db.Column(db.Text(), nullable=True)
+    target_commit_hash = models.TextField(blank=True, default="")
 
-    revisions: list[Revision] = db.relationship(
-        "Revision",
-        secondary=revision_landing_job,
-        back_populates="landing_jobs",
-        order_by="revision_landing_job.columns.index",
-    )
+    revisions = models.ManyToManyField(Revision, through="RevisionLandingJob")
+
+    # These are automatically set, deprecated fields, but kept for compatibility.
+    repository_name = models.TextField(default="", blank=True)
+    repository_url = models.TextField(default="", blank=True)
+
+    # New field in lieu of deprecated repository fields.
+    target_repo = models.ForeignKey("Repo", on_delete=models.SET_NULL, null=True)
 
     @property
     def landed_revisions(self) -> dict:
         """Return revision and diff ID mapping associated with the landing job."""
         revision_ids = [revision.id for revision in self.revisions]
-        revision_to_diff_ids_query = (
-            revision_landing_job.select()
-            .join(Revision)
-            .where(
-                revision_landing_job.c.revision_id.in_(revision_ids),
-                revision_landing_job.c.landing_job_id == self.id,
+        revision_landing_jobs = (
+            RevisionLandingJob.objects.filter(
+                revision__id__in=revision_ids,
+                landing_job=self,
             )
-            .with_only_columns(Revision.revision_id, revision_landing_job.c.diff_id)
-            .order_by(revision_landing_job.c.index)
+            .order_by("index")
+            .values("revision_id", "landing_job_id", "diff_id")
         )
-        return dict(list(db.session.execute(revision_to_diff_ids_query)))
+        return dict(list(revision_landing_jobs))
 
     @property
     def serialized_landing_path(self):
@@ -179,20 +148,20 @@ class LandingJob(Base):
         Returns a Phabricator revision ID if the revisions are associated with a Phabricator
         repo, otherwise the first line of the commit message.
         """
-        if not self.revisions:
+        if not self.revisions.exists():
             raise ValueError(
                 "Job must be associated with a revision to have a relevant identifier."
             )
 
-        head = self.revisions[-1]
+        head = self.revisions.order_by("id").first()
         if head.revision_id:
             # Return the Phabricator identifier if the head revision has one.
             return f"D{head.revision_id}"
 
         # If there is no Phabricator identifier, return the first line of the
         # non-try-syntax commit's message for the patch.
-        if len(self.revisions) > 1:
-            head = self.revisions[-2]
+        if self.revisions.count() > 1:
+            head = self.revisions.order_by("-id")[1]
 
         commit_message = head.patch_data.get("commit_message")
         if commit_message:
@@ -202,29 +171,23 @@ class LandingJob(Base):
         return "unknown"
 
     @classmethod
-    def revisions_query(cls, revisions: Iterable[str]) -> flask_sqlalchemy.BaseQuery:
+    def revisions_query(cls, revisions: Iterable[str]) -> QuerySet:
         """
         Return all landing jobs associated with a given list of revisions.
 
         Older records do not have associated revisions, but rather have a JSONB field
-        that stores revisions and diff IDs. Both associated revisions and revisions that
-        appear in the revision_to_diff_id are used to fetch landing jobs.
+        that stores revisions and diff IDs. Those records are now deprecated and will
+        not be included in this query.
         """
-
         revisions = [str(int(r)) for r in revisions]
-        return cls.query.filter(
-            or_(
-                cls.revisions.any(Revision.revision_id.in_(revisions)),
-                cls.revision_to_diff_id.has_any(array(revisions)),
-            )
-        )
+        return cls.query.filter(revision_id__in=revisions)
 
     @classmethod
     def job_queue_query(
         cls,
         repositories: Optional[Iterable[str]] = None,
         grace_seconds: int = DEFAULT_GRACE_SECONDS,
-    ) -> flask_sqlalchemy.BaseQuery:
+    ) -> QuerySet:
         """Return a query which selects the queued jobs.
 
         Args:
@@ -238,41 +201,37 @@ class LandingJob(Base):
             LandingJobStatus.IN_PROGRESS,
             LandingJobStatus.DEFERRED,
         )
-        q = cls.query.filter(cls.status.in_(applicable_statuses))
+        q = cls.objects.filter(status__in=applicable_statuses)
 
         if repositories:
-            q = q.filter(cls.repository_name.in_(repositories))
+            q = q.filter(target_repo__in=repositories)
 
         if grace_seconds:
             now = datetime.datetime.now(datetime.timezone.utc)
             grace_cutoff = now - datetime.timedelta(seconds=grace_seconds)
-            q = q.filter(cls.created_at < grace_cutoff)
+            q = q.filter(created_at__lt=grace_cutoff)
 
         # Any `LandingJobStatus.IN_PROGRESS` job is first and there should
         # be a maximum of one (per repository). For
         # `LandingJobStatus.SUBMITTED` jobs, higher priority items come first
         # and then we order by creation time (older first).
-        q = q.order_by(cls.status.desc(), cls.priority.desc(), cls.created_at)
+        q = q.order_by("-status", "-priority", "created_at")
 
         return q
 
     @classmethod
-    def next_job_for_update_query(
-        cls, repositories: Optional[Iterable[str]] = None
-    ) -> flask_sqlalchemy.BaseQuery:
+    def next_job(cls, repositories: Optional[Iterable[str]] = None) -> QuerySet:
         """Return a query which selects the next job and locks the row."""
         query = cls.job_queue_query(repositories=repositories)
 
         # Returned rows should be locked for updating, this ensures the next
         # job can be claimed.
-        query = query.with_for_update()
-
-        return query
+        return query.select_for_update()
 
     def add_revisions(self, revisions: list[Revision]):
         """Associate a list of revisions with job."""
         for revision in revisions:
-            self.revisions.append(revision)
+            self.revisions.add(revision)
 
     def sort_revisions(self, revisions: list[Revision]):
         """Sort the associated revisions based on provided list."""
@@ -281,27 +240,17 @@ class LandingJob(Base):
 
         # Update association table records with correct index values.
         for index, revision in enumerate(revisions):
-            db.session.execute(
-                revision_landing_job.update()
-                .where(revision_landing_job.c.landing_job_id == self.id)
-                .where(
-                    revision_landing_job.c.revision_id == revision.id,
-                )
-                .values(index=index)
-            )
+            RevisionLandingJob.objects.filter(
+                revision=revision, landing_job=self
+            ).update(index=index)
 
     def set_landed_revision_diffs(self):
         """Assign diff_ids, if available, to each association row."""
         # Update association table records with current diff_id values.
         for revision in self.revisions:
-            db.session.execute(
-                revision_landing_job.update()
-                .where(
-                    revision_landing_job.c.landing_job_id == self.id,
-                    revision_landing_job.c.revision_id == revision.id,
-                )
-                .values(diff_id=revision.diff_id)
-            )
+            RevisionLandingJob.objects.filter(
+                revision=revision, landing_job=self
+            ).update(diff_id=revision.diff_id)
 
     def set_landed_reviewers(self, path: Path):
         """Set approving peers and owners at time of landing."""
@@ -321,23 +270,17 @@ class LandingJob(Base):
                 ):
                     revision.data["peers_and_owners"].append(reviewer)
 
-            # Mark revision.data as "modified" since SQLAlchemy does not seem to
-            # be detecting changes on JSONB fields.
-            flag_modified(revision, "data")
+            revision.save()
 
     def transition_status(
         self,
         action: LandingJobAction,
-        commit: bool = False,
-        db: Optional[flask_sqlalchemy.SQLAlchemy] = None,
         **kwargs,
     ):
         """Change the status and other applicable fields according to actions.
 
         Args:
             action (LandingJobAction): the action to take, e.g. "land" or "fail"
-            commit (bool): whether to commit the changes to the database or not
-            db (SQLAlchemy.db): the database to commit to
             **kwargs:
                 Additional arguments required by each action, e.g. `message` or
                 `commit_id`.
@@ -369,9 +312,6 @@ class LandingJob(Base):
             missing_params = required_params - kwargs.keys()
             raise ValueError(f"Missing {missing_params} params")
 
-        if commit and db is None:
-            raise ValueError("db is required when commit is set to True")
-
         self.status = actions[action]["status"]
 
         if action in (LandingJobAction.FAIL, LandingJobAction.DEFER):
@@ -380,8 +320,7 @@ class LandingJob(Base):
         if action == LandingJobAction.LAND:
             self.landed_commit_id = kwargs["commit_id"]
 
-        if commit:
-            db.session.commit()
+        self.save()
 
     def serialize(self) -> dict[str, Any]:
         """Return a JSON compatible dictionary."""
@@ -410,7 +349,7 @@ class LandingJob(Base):
 def add_job_with_revisions(revisions: list[Revision], **params: Any) -> LandingJob:
     """Creates a new job and associates provided revisions with it."""
     job = LandingJob(**params)
-    db.session.add(job)
+    job.save()
     add_revisions_to_job(revisions, job)
     return job
 
@@ -418,6 +357,4 @@ def add_job_with_revisions(revisions: list[Revision], **params: Any) -> LandingJ
 def add_revisions_to_job(revisions: list[Revision], job: LandingJob):
     """Given an existing job, add and sort provided revisions."""
     job.add_revisions(revisions)
-    db.session.commit()
     job.sort_revisions(revisions)
-    db.session.commit()
