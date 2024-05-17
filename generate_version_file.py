dd7afea79cd0ef7a3312ccccb691e4d79aa53075
Alex Hochheiden <ahochheiden@mozilla.com>
2024-05-17 10:16:56 -0700

    version: Address code review comments

--- generate_version_file.py
+++
@@ -1,3 +0,0 @@
-from setuptools_scm import get_version
-
-get_version(root="./code/", relative_to="__file__", write_to="src/lando/version.py")
--- src/lando/utils/management/commands/generate_version_file.py
+++ src/lando/utils/management/commands/generate_version_file.py
@@ -1,6 +1,3 @@
-import subprocess
-import sys
-
 from django.core.management.base import BaseCommand
 
 
@@ -8,4 +5,6 @@ class Command(BaseCommand):
     help = "Explicitly generate the 'version.py' file."
 
     def handle(self, *args, **options):
-        subprocess.call([sys.executable, "code/generate_version_file.py"])
+        from setuptools_scm import get_version
+
+        get_version(write_to="src/lando/version.py")
