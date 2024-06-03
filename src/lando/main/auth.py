a251266cba1b968da530b71729c95723e2810f17
Zeid Zabaneh <zeid@mozilla.com>
2024-06-03 14:49:09 -0400

    auth: add scm permissions to model, sync (bug 1899395)
    - add scm permission to the Profile model
    - sync permissions during auth cycle

--- src/lando/main/auth.py
+++ src/lando/main/auth.py
@@ -1,4 +1,7 @@
+from django.contrib.auth.models import User
 from mozilla_django_oidc.auth import OIDCAuthenticationBackend
+
+from lando.main.models.profile import Profile
 
 
 class LandoOIDCAuthenticationBackend(OIDCAuthenticationBackend):
@@ -10,6 +13,13 @@ class LandoOIDCAuthenticationBackend(OIDCAuthenticationBackend):
         return self.userinfo
 
     def update_user(self, user, claims):
-        user.profile.userinfo = self.userinfo
-        user.profile.save()
+        try:
+            user_profile = user.profile
+        except User.profile.RelatedObjectDoesNotExist:
+            user_profile = Profile(user=user)
+            user_profile.save()
+
+        user_profile.userinfo = self.userinfo
+        user_profile.save()
+        user_profile.update_permissions()
         return super().update_user(user, claims)
--- src/lando/main/models/profile.py
+++ src/lando/main/models/profile.py
@@ -1,12 +1,24 @@
 from __future__ import annotations
 
-from django.contrib.auth.models import User
+from django.contrib.auth.models import Permission, User
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 from lando.main.models.base import BaseModel
 
+SCM_PERMISSIONS = (
+    ("scm_conduit", "SCM_CONDUIT"),
+    ("scm_level_1", "SCM_LEVEL_1"),
+    ("scm_level_2", "SCM_LEVEL_2"),
+    ("scm_level_3", "SCM_LEVEL_3"),
+    ("scm_versioncontrol", "SCM_VERSIONCONTROL"),
+)
+
 
 class Profile(BaseModel):
+    class Meta:
+        permissions = SCM_PERMISSIONS
+
     user = models.OneToOneField(User, null=True, on_delete=models.SET_NULL)
 
     # Phabricator API token.
@@ -14,3 +26,19 @@ class Profile(BaseModel):
 
     # User info fetched from SSO.
     userinfo = models.JSONField(default=dict, blank=True)
+
+    def update_permissions(self):
+        self.user.user_permissions.remove(
+            **[permission[0] for permission in SCM_PERMISSIONS]
+        )
+
+        groups = self.userinfo.get("https://sso.mozilla.com/claim/groups", [])
+        content_type = ContentType.objects.get_for_model(self.__class__)
+
+        for codename in SCM_PERMISSIONS:
+            permission = Permission.objects.get(
+                codename=codename,
+                content_type=content_type,
+            )
+            if f"all_{permission}" in groups and f"expired_{permission}" not in groups:
+                self.user.user_permissions.add(permission)
