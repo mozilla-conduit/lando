7015f148ec86648c57f9359fa88f6e55027834fb
Zeid Zabaneh <zeid@mozilla.com>
2024-06-04 08:57:07 -0400

    add code comments

--- src/lando/main/auth.py
+++ src/lando/main/auth.py
@@ -8,17 +8,23 @@ class LandoOIDCAuthenticationBackend(OIDCAuthenticationBackend):
     """An extended OIDC auth backend that manipulates the user profile."""
 
     def get_userinfo(self, *args, **kwargs):
+        # Store fetched userinfo on this instance.
         self.userinfo = super().get_userinfo(*args, **kwargs)
         return self.userinfo
 
     def update_user(self, user, claims):
+        # Check if there is an existing user profile, create it if not.
         try:
             user_profile = user.profile
         except User.profile.RelatedObjectDoesNotExist:
             user_profile = Profile(user=user)
             user_profile.save()
 
+        # Store user info in the user profile.
         user_profile.userinfo = self.userinfo
         user_profile.save()
+
+        # Update user permissions.
         user_profile.update_permissions()
+
         return super().update_user(user, claims)
