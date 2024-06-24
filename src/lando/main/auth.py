2f3a366684292977ac2c710d16f120e82cebb464
Zeid Zabaneh <zeid@mozilla.com>
2024-06-24 15:59:37 -0400

    code review feedback

--- src/lando/main/auth.py
+++ src/lando/main/auth.py
@@ -8,7 +8,7 @@ class LandoOIDCAuthenticationBackend(OIDCAuthenticationBackend):
     """An extended OIDC auth backend that manipulates the user profile."""
 
     @staticmethod
-    def _update_user_profile_permissions(user_profile, claims):
+    def _update_user_profile_permissions(user_profile: Profile, claims: dict):
         # Store user info in the user profile.
         user_profile.userinfo = claims
         user_profile.save()
@@ -17,7 +17,7 @@ class LandoOIDCAuthenticationBackend(OIDCAuthenticationBackend):
         user_profile.update_permissions()
 
     @staticmethod
-    def _get_or_create_user_profile(user):
+    def _get_or_create_user_profile(user: User) -> Profile:
         # Check if there is an existing user profile, create it if not.
         try:
             user_profile = user.profile
@@ -26,16 +26,16 @@ class LandoOIDCAuthenticationBackend(OIDCAuthenticationBackend):
             user_profile.save()
         return user_profile
 
-    def post_auth_hook(self, user, claims):
+    def post_auth_hook(self, user: User, claims: dict):
         """Perform user profile related tasks upon login."""
         user_profile = self._get_or_create_user_profile(user)
         self._update_user_profile_permissions(user_profile, claims)
 
-    def create_user(self, claims):
+    def create_user(self, claims: dict) -> User:
         user = super().create_user(claims)
         self.post_auth_hook(user, claims)
         return user
 
-    def update_user(self, user, claims):
+    def update_user(self, user: User, claims: dict) -> User:
         self.post_auth_hook(user, claims)
         return super().update_user(user, claims)
