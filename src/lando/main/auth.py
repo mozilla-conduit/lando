102eaf1933f3c199bfb16961128a20c235a69fbc
Zeid Zabaneh <2043828+zzzeid@users.noreply.github.com>
2024-06-25 08:21:38 -0400

    auth: trigger user profile creation on first login (bug 1903802) (#80)
    - add `_update_user_profile_permissions` method
    - add `_get_or_create_user_profile` method
    - add `post_auth_hook` method that calls above two methods
    - call `post_auth_hook` in `create_user` and `update_user`

--- src/lando/main/auth.py
+++ src/lando/main/auth.py
@@ -7,14 +7,8 @@ from lando.main.models.profile import Profile
 class LandoOIDCAuthenticationBackend(OIDCAuthenticationBackend):
     """An extended OIDC auth backend that manipulates the user profile."""
 
-    def update_user(self, user, claims):
-        # Check if there is an existing user profile, create it if not.
-        try:
-            user_profile = user.profile
-        except User.profile.RelatedObjectDoesNotExist:
-            user_profile = Profile(user=user)
-            user_profile.save()
-
+    @staticmethod
+    def _update_user_profile_permissions(user_profile: Profile, claims: dict):
         # Store user info in the user profile.
         user_profile.userinfo = claims
         user_profile.save()
@@ -22,4 +16,26 @@ class LandoOIDCAuthenticationBackend(OIDCAuthenticationBackend):
         # Update user permissions.
         user_profile.update_permissions()
 
+    @staticmethod
+    def _get_or_create_user_profile(user: User) -> Profile:
+        # Check if there is an existing user profile, create it if not.
+        try:
+            user_profile = user.profile
+        except User.profile.RelatedObjectDoesNotExist:
+            user_profile = Profile(user=user)
+            user_profile.save()
+        return user_profile
+
+    def post_auth_hook(self, user: User, claims: dict):
+        """Perform user profile related tasks upon login."""
+        user_profile = self._get_or_create_user_profile(user)
+        self._update_user_profile_permissions(user_profile, claims)
+
+    def create_user(self, claims: dict) -> User:
+        user = super().create_user(claims)
+        self.post_auth_hook(user, claims)
+        return user
+
+    def update_user(self, user: User, claims: dict) -> User:
+        self.post_auth_hook(user, claims)
         return super().update_user(user, claims)
