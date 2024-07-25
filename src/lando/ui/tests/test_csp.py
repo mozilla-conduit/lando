5f909bf86a9be2bd11a23bc435228781c5702503
Zeid Zabaneh <2043828+zzzeid@users.noreply.github.com>
2024-07-25 16:05:33 -0400

    misc: remove license headers from legacy api files (bug 1898318) (#66)

--- src/lando/ui/tests/test_csp.py
+++ src/lando/ui/tests/test_csp.py
@@ -1,8 +1,3 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-
 def test_csp_headers_set(client):
     response = client.get("/")
     assert "Content-Security-Policy" in response.headers
