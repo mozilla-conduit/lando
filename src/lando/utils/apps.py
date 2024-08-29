c601cdec0bf2fd810d935b96f2e9aa3027ef28c4
Zeid Zabaneh <zeid@mozilla.com>
2024-08-29 11:25:04 -0400

    code review feedback

--- src/lando/utils/apps.py
+++ src/lando/utils/apps.py
@@ -8,6 +8,10 @@ from lando.utils.phabricator import PhabricatorAPIException, PhabricatorClient
 
 PHAB_API_KEY_RE = re.compile(r"^api-.{28}$")
 
+PHAB_API_KEY_FORMAT_ERROR_TEMPLATE = (
+    '{} has the wrong format it must begin with "api-" and be 32 characters long.'
+)
+
 
 class UtilsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
@@ -19,32 +23,34 @@ def phabricator_check(**kwargs) -> list[Error]:
     """Check validity of Phabricator settings and check connectivity."""
     errors = []
 
-    unpriv_key = settings.PHABRICATOR_UNPRIVILEGED_API_KEY
-    priv_key = settings.PHABRICATOR_ADMIN_API_KEY
+    unprivileged_key = settings.PHABRICATOR_UNPRIVILEGED_API_KEY
+    admin_key = settings.PHABRICATOR_ADMIN_API_KEY
 
-    if unpriv_key and PHAB_API_KEY_RE.search(unpriv_key) is None:
+    if unprivileged_key and PHAB_API_KEY_RE.search(unprivileged_key) is None:
         errors.append(
-            "PHABRICATOR_UNPRIVILEGED_API_KEY has the wrong format, "
-            'it must begin with "api-" and be 32 characters long.'
+            PHAB_API_KEY_FORMAT_ERROR_TEMPLATE.format(
+                "PHABRICATOR_UNPRIVILEGED_API_KEY"
+            )
         )
 
-    if priv_key and PHAB_API_KEY_RE.search(priv_key) is None:
+    if admin_key and PHAB_API_KEY_RE.search(admin_key) is None:
         errors.append(
-            "PHABRICATOR_ADMIN_API_KEY has the wrong format, "
-            'it must begin with "api-" and be 32 characters long.'
+            PHAB_API_KEY_FORMAT_ERROR_TEMPLATE.format("PHABRICATOR_ADMIN_API_KEY")
         )
 
-    if (unpriv_key or priv_key) and not errors:
-        if unpriv_key:
+    if (unprivileged_key or admin_key) and not errors:
+        if unprivileged_key:
             try:
                 PhabricatorClient(
                     settings.PHABRICATOR_URL,
                     settings.PHABRICATOR_UNPRIVILEGED_API_KEY,
                 ).call_conduit("conduit.ping")
             except PhabricatorAPIException as e:
-                errors.append(f"PhabricatorAPIException: {e!s} (using unpriviledged key)")
+                errors.append(
+                    f"PhabricatorAPIException: {e!s} (using unpriviledged key)"
+                )
 
-        if priv_key:
+        if admin_key:
             try:
                 PhabricatorClient(
                     settings.PHABRICATOR_URL,
