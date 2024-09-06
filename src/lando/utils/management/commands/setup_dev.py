f8827fdeb1ef1c0a6bad1ab799d896081ada5b6a
Zeid Zabaneh <2043828+zzzeid@users.noreply.github.com>
2024-09-06 12:28:35 -0400

    commands: update setup_dev to add admin user (bug 1917078) (#120)
    - add method that creates or updates admin user on local environment
    - split handling of functionality into individual methods

--- src/lando/utils/management/commands/setup_dev.py
+++ src/lando/utils/management/commands/setup_dev.py
@@ -1,4 +1,6 @@
-from django.core.management.base import BaseCommand
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand, CommandError
 
 from lando.main.models import Repo, Worker
 
@@ -6,7 +8,8 @@ from lando.main.models import Repo, Worker
 class Command(BaseCommand):
     help = "Generate required database records used in dev."
 
-    def handle(self, *args, **options):
+    def setup_workers(self):
+        """Ensure a git and an hg worker exist on the local environment."""
         # Set up two workers, one for each SCM.
         workers = {
             Repo.HG: None,
@@ -30,4 +33,27 @@ class Command(BaseCommand):
             self.stdout.write(f"Adding {repo} ({repo.scm}) to {workers[repo.scm]}.")
             workers[repo.scm].applicable_repos.add(repo)
 
+    def setup_users(self):
+        """Ensure there is an administrator account on the local system."""
+        # In case someone is trying to run this manually for whatever reason on a
+        # different environment, raise an exception so an admin user with a weak
+        # password is not accidentally created.
+        if settings.ENVIRONMENT != "local":
+            raise CommandError("This method can not be triggered on this environment.")
+
+        try:
+            user = User.objects.get(username="admin")
+            self.stdout.write(f"Admin user ({user}) found, resetting settings.")
+        except User.DoesNotExist:
+            user = User.objects.create_user("admin", password="password")
+            self.stdout.write(f"Admin user ({user}) created.")
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+    def handle(self, *args, **options):
+        if settings.ENVIRONMENT != "local":
+            raise CommandError("This script can only be run on a local environment.")
+        self.setup_workers()
+        self.setup_users()
         self.stdout.write("Finished.")
