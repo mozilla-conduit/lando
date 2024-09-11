46913c3625f173c0d69c27b0ff019eaf3c690876
Zeid Zabaneh <2043828+zzzeid@users.noreply.github.com>
2024-09-11 13:33:55 -0400

    sentry: port sentry support (bug 1873221) (#125)

--- src/lando/main/apps.py
+++ src/lando/main/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
 
+from lando.main.sentry import init_sentry
+
 
 class MainConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "lando.main"
+
+    def ready(self):
+        """Run initialization tasks."""
+        init_sentry()
