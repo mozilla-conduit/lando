f6bbbdfb78ae70dd0b0e66f5a75a7277d41d2907
Zeid Zabaneh <zeid@mozilla.com>
2024-10-29 14:34:59 +1100

    landing_jobs: fix job cancelling (bug 1915712)

--- src/lando/api/legacy/api/landing_jobs.py
+++ src/lando/api/legacy/api/landing_jobs.py
@@ -1,5 +1,7 @@
+import json
 import logging
 
+from django import forms
 from django.http import Http404, HttpRequest, JsonResponse
 
 from lando.main.auth import require_authenticated_user
@@ -8,8 +10,18 @@ from lando.main.models.landing_job import LandingJob, LandingJobAction, LandingJ
 logger = logging.getLogger(__name__)
 
 
+class LandingJobForm(forms.Form):
+    """Simple form to clean API endpoint fields."""
+
+    # NOTE: this is here as a quick solution to safely check and clean user input,
+    # however it will likely be deprecated in favour of a more universal solution
+    # as part of bug 1870097.
+    landing_job_id = forms.IntegerField()
+    status = forms.CharField()
+
+
 @require_authenticated_user
-def put(request: HttpRequest, landing_job_id: str, data: dict):
+def put(request: HttpRequest, landing_job_id: str):
     """Update a landing job.
 
     Checks whether the logged in user is allowed to modify the landing job that is
@@ -27,11 +39,27 @@ def put(request: HttpRequest, landing_job_id: str, data: dict):
             updated (for example, when trying to cancel a job that is already in
             progress).
     """
-    with LandingJob.lock_table:
-        landing_job = LandingJob.objects.get(pk=landing_job_id)
+    data = json.loads(request.body)
+    data["landing_job_id"] = landing_job_id
+    form = LandingJobForm(data)
 
-    if not landing_job:
-        raise Http404(f"A landing job with ID {landing_job_id} was not found.")
+    if not form.is_valid():
+        data = {
+            "errors": [
+                f"{field}: {', '.join(field_errors)}"
+                for field, field_errors in form.errors.items()
+            ]
+        }
+        return JsonResponse(data, status=400)
+
+    landing_job_id = form.cleaned_data["landing_job_id"]
+    status = form.cleaned_data["status"]
+
+    with LandingJob.lock_table:
+        try:
+            landing_job = LandingJob.objects.get(pk=landing_job_id)
+        except LandingJob.DoesNotExist:
+            raise Http404(f"A landing job with ID {landing_job_id} was not found.")
 
     ldap_username = request.user.email
     if landing_job.requester_email != ldap_username:
@@ -39,19 +67,18 @@ def put(request: HttpRequest, landing_job_id: str, data: dict):
             f"User not authorized to update landing job {landing_job_id}"
         )
 
-    # TODO: fix this. See bug 1893455.
-    if data["status"] != "CANCELLED":
-        data = {"errors": [f"The provided status {data['status']} is not allowed."]}
-        return JsonResponse(data, status_code=400)
+    if status != "CANCELLED":
+        data = {"errors": [f"The provided status {status} is not allowed."]}
+        return JsonResponse(data, status=400)
 
     if landing_job.status in (LandingJobStatus.SUBMITTED, LandingJobStatus.DEFERRED):
         landing_job.transition_status(LandingJobAction.CANCEL)
         landing_job.save()
-        return {"id": landing_job.id}, 200
+        return JsonResponse({"id": landing_job.id})
     else:
         data = {
             "errors": [
                 f"Landing job status ({landing_job.status}) does not allow cancelling."
             ]
         }
-        return JsonResponse(data, status_code=400)
+        return JsonResponse(data, status=400)
