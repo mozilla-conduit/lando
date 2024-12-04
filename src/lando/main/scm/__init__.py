e76b159b654c8f06928d5337047179aa02a05daf
Olivier Mehani <omehani@mozilla.com>
2024-12-04 16:49:11 +1100

    fixup! scm: use SCM rather than Scm in class names

--- src/lando/main/scm/__init__.py
+++ src/lando/main/scm/__init__.py
@@ -1,8 +1,8 @@
 # flake8: noqa
-from .abstract_scm import *
-from .consts import *
-from .exceptions import *
-from .hg import *
+from lando.main.scm.abstract_scm import *
+from lando.main.scm.consts import *
+from lando.main.scm.exceptions import *
+from lando.main.scm.hg import *
 
 SCM_IMPLEMENTATIONS = {
     # SCM_GIT: GitScm,
