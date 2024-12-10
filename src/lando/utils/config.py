d10ddb437373bb804de1dd9c56c5c3c4d337f2af
Olivier Mehani <omehani@mozilla.com>
2024-12-10 14:30:10 +1100

    squash! landing_worker: move read_lando_config to lando.utils.config
    Also, simplify the signature to not have Optionals

--- src/lando/utils/config.py
+++ src/lando/utils/config.py
@@ -1,12 +1,8 @@
 from configparser import ConfigParser
-from typing import Optional
 
 
-def read_lando_config(lando_ini_contents: Optional[str]) -> Optional[ConfigParser]:
+def read_lando_config(lando_ini_contents: str) -> ConfigParser:
     """Attempt to read the `.lando.ini` file."""
-    if not lando_ini_contents:
-        return None
-
     # ConfigParser will use `:` as a delimeter unless told otherwise.
     # We set our keys as `formatter:pattern` so specify `=` as the delimiters.
     parser = ConfigParser(delimiters="=")
