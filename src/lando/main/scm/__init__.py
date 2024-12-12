0bffccb7bb65221ff7b4eeb1db1f39c7ac0344d0
Olivier Mehani <omehani@mozilla.com>
2024-12-12 11:53:46 +1100

    scm: explicitly export module objects
    (cherry picked from commit 6ac8ebc691d85834a0612fab89a72c7f1bbe74f0)

--- src/lando/main/scm/__init__.py
+++ src/lando/main/scm/__init__.py
@@ -1,9 +1,34 @@
-# flake8: noqa
-from lando.main.scm.abstract_scm import *
-from lando.main.scm.consts import *
-from lando.main.scm.exceptions import *
-from lando.main.scm.git import *
-from lando.main.scm.hg import *
+from lando.main.scm.abstract_scm import AbstractSCM
+from lando.main.scm.consts import (
+    SCM_GIT,
+    SCM_HG,
+)
+from lando.main.scm.exceptions import (
+    AutoformattingException,
+    NoDiffStartLine,
+    PatchApplicationFailure,
+    PatchConflict,
+    SCMException,
+    SCMInternalServerError,
+    SCMLostPushRace,
+    SCMPushTimeoutException,
+    TreeApprovalRequired,
+    TreeClosed,
+)
+from lando.main.scm.git import GitSCM
+from lando.main.scm.hg import (
+    REQUEST_USER_ENV_VAR,
+    HgCommandError,
+    HgException,
+    HgmoInternalServerError,
+    HgPatchConflict,
+    HgSCM,
+    HgTreeApprovalRequired,
+    HgTreeClosed,
+    LostPushRace,
+    PushTimeoutException,
+    hglib,
+)
 
 # These can only be determined when all the subclasses of the AbstractSCM have been defined.
 SCM_CHOICES = {
@@ -12,3 +37,39 @@ SCM_CHOICES = {
 SCM_IMPLEMENTATIONS = {
     klass.scm_type(): klass for klass in AbstractSCM.__subclasses__()
 }
+
+__all__ = [
+    # abstract_scm
+    AbstractSCM,
+    # consts
+    SCM_HG,
+    SCM_GIT,
+    # consts (built up)
+    SCM_CHOICES,
+    SCM_IMPLEMENTATIONS,
+    # exceptions
+    SCMException,
+    AutoformattingException,
+    PatchApplicationFailure,
+    NoDiffStartLine,
+    PatchConflict,
+    SCMInternalServerError,
+    SCMLostPushRace,
+    SCMPushTimeoutException,
+    TreeApprovalRequired,
+    TreeClosed,
+    # git
+    GitSCM,
+    # hg
+    hglib,
+    HgException,
+    HgCommandError,
+    HgTreeClosed,
+    HgTreeApprovalRequired,
+    LostPushRace,
+    PushTimeoutException,
+    HgmoInternalServerError,
+    HgPatchConflict,
+    HgSCM,
+    REQUEST_USER_ENV_VAR,
+]
