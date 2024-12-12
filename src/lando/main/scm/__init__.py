f29120bdd318c750e15a9ccc525b06502acb16a6
Olivier Mehani <omehani@mozilla.com>
2024-12-12 11:53:46 +1100

    scm: explicitly export module objects

--- src/lando/main/scm/__init__.py
+++ src/lando/main/scm/__init__.py
@@ -1,10 +1,58 @@
-# flake8: noqa
-from lando.main.scm.abstract_scm import *
-from lando.main.scm.consts import *
-from lando.main.scm.exceptions import *
-from lando.main.scm.hg import *
+from lando.main.scm.abstract_scm import AbstractSCM
+from lando.main.scm.consts import (
+    SCM_CHOICES,
+    SCM_GIT,
+    SCM_HG,
+)
+from lando.main.scm.exceptions import (
+    AutoformattingException,
+    NoDiffStartLine,
+    PatchApplicationFailure,
+    PatchConflict,
+    SCMException,
+    SCMInternalServerError,
+    SCMLostPushRace,
+    SCMPushTimeoutException,
+    TreeApprovalRequired,
+    TreeClosed,
+)
+from lando.main.scm.hg import (
+    REQUEST_USER_ENV_VAR,
+    HgCommandError,
+    HgException,
+    HgSCM,
+    hglib,
+)
 
 SCM_IMPLEMENTATIONS = {
     # SCM_GIT: GitSCM,
     SCM_HG: HgSCM,
 }
+
+__all__ = [
+    # abstract_scm
+    "AbstractSCM",
+    # consts
+    "SCM_HG",
+    "SCM_GIT",
+    # consts (built up)
+    "SCM_CHOICES",
+    "SCM_IMPLEMENTATIONS",
+    # exceptions
+    "SCMException",
+    "AutoformattingException",
+    "PatchApplicationFailure",
+    "NoDiffStartLine",
+    "PatchConflict",
+    "SCMInternalServerError",
+    "SCMLostPushRace",
+    "SCMPushTimeoutException",
+    "TreeApprovalRequired",
+    "TreeClosed",
+    # hg
+    "hglib",
+    "HgException",
+    "HgCommandError",
+    "HgSCM",
+    "REQUEST_USER_ENV_VAR",
+]
