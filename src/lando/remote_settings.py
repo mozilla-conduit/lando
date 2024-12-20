3adf5e62c5745a21d59ad501fd089dfc2cef3016
Zeid <2043828+zzzeid@users.noreply.github.com>
2024-12-20 08:55:04 -0500

    remote_settings: add email configuration (bug 1898324) (#181)
    - add settings for SMTP configuration in remote environments
    - set default console backend for local environment

--- src/lando/remote_settings.py
+++ src/lando/remote_settings.py
@@ -39,3 +39,12 @@ LOGGING = {
     "root": {"handlers": ["null"]},
     "disable_existing_loggers": True,
 }
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_TIMEOUT = 60
