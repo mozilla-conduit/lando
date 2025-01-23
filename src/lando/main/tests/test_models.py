8ff9b8f13eeb37863da2dbdc193615e580365764
Olivier Mehani <omehani@mozilla.com>
2025-01-23 12:01:20 +1100

    revision: add individual properties for patch metadata (bug 1936171)
    The ultimate goal of this change is to deprecate the back-and-forth use
    of the HgPatchHelper. This first change allows to confine the parsing of
    an Hg patch to the Revision object, which allows exposing cleaned-up
    attribute to the rest of the system.

--- src/lando/main/tests/test_models.py
+++ src/lando/main/tests/test_models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from unittest.mock import MagicMock, patch
 
 import pytest
@@ -5,10 +6,20 @@ from django.conf import settings
 from django.core.exceptions import ValidationError
 
 from lando.main.models import Repo
+from lando.main.models.revision import Revision
 from lando.main.scm import (
     SCM_TYPE_GIT,
     SCM_TYPE_HG,
 )
+
+DIFF_ONLY = """
+diff --git a/test.txt b/test.txt
+--- a/test.txt
++++ b/test.txt
+@@ -1,1 +1,2 @@
+ TEST
++adding another line
+""".lstrip()
 
 
 @pytest.mark.parametrize(
@@ -84,3 +95,64 @@ def test__models__Repo__system_path_validator(path, expected_exception):
             repo.clean_fields()
     else:
         repo.clean_fields()  # Should not raise any exception
+
+
+@pytest.mark.parametrize(
+    "author, expected",
+    [
+        (
+            "A. Uthor <author@moz.test>",
+            ("A. Uthor", "author@moz.test"),
+        ),
+        (
+            "author@moz.test",
+            ("", "author@moz.test"),
+        ),
+        (
+            "<author@moz.test>",
+            ("", "author@moz.test"),
+        ),
+        (
+            "A. Uthor",
+            ("A. Uthor", ""),
+        ),
+        (
+            "@ Uthor",
+            ("@ Uthor", ""),
+        ),
+        (
+            "<@ Uthor>",
+            ("<@ Uthor>", ""),
+        ),
+    ],
+)
+def test__models__Revision___parse_author_string(author, expected):
+    assert Revision._parse_author_string(author) == expected
+
+
+@pytest.mark.django_db()
+def test__models__Revision__metadata():
+    author = "A. Uthor"
+    email = "author@moz.test"
+    commit_message = """Multiline Commit Message
+
+    More lines
+    """
+    timestamp = datetime.now().strftime("%s")
+
+    r = Revision.new_from_patch(
+        raw_diff=DIFF_ONLY,
+        patch_data={
+            "author_name": author,
+            "author_email": email,
+            "commit_message": commit_message,
+            "timestamp": timestamp,
+        },
+    )
+
+    assert r.author_name == author
+    assert r.author_email == email
+    assert r.author == f"{author} <{email}>"
+    assert r.commit_message == commit_message
+    assert r.timestamp == timestamp
+    assert r.diff == DIFF_ONLY
