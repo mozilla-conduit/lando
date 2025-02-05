b96ee4dbf1266f3f27a38111895225306d132b47
Olivier Mehani <omehani@mozilla.com>
2025-02-05 17:52:13 +1100

    fixup! pulse: wip

--- src/lando/pulse/tests/test_pulse.py
+++ src/lando/pulse/tests/test_pulse.py
@@ -1,10 +1,21 @@
+from unittest import mock
+
 import kombu
-from pytest import mock
+import pytest
 
 from lando.pulse.pulse import PulseNotifier
 
+# We need to import dependencies of the fixtures we use, even if we don't use those
+# directly.
+from lando.pushlog.tests.conftest import make_commit, make_hash, make_push, make_repo
 
-def test__PulseNotifier(make_push, monkeypatch):
+# We need some local usage of those imported fixtures to satisfy the linters.
+# This is it.
+__all__ = ["make_commit", "make_hash", "make_push", "make_repo"]
+
+
+@pytest.mark.django_db
+def test__PulseNotifier(make_repo, make_commit, make_push, monkeypatch):
     connection = kombu.Connection("amqp://")
     notifier = PulseNotifier(connection)
 
@@ -14,7 +25,9 @@ def test__PulseNotifier(make_push, monkeypatch):
         publish,
     )
 
-    push = make_push()
+    repo = make_repo(1)
+    commit = make_commit(repo=repo, seqno=1)
+    push = make_push(repo=repo, commits=[commit])
 
     notifier.notify_push(push)
 
