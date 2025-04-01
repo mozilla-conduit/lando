5e1db1979be417c564adf767a7d813c520c373d6
Connor Sheehan <cosheehan@mozilla.com>
2025-04-01 11:00:48 -0400

    add create_api_token.py management command

---
+++ src/lando/headless_api/management/commands/create_api_token.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+import logging
+from argparse import ArgumentParser
+
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand, CommandError
+
+from lando.headless_api.models.tokens import ApiToken
+
+logger = logging.getLogger(__name__)
+
+class Command(BaseCommand):
+    help = "Create an API token for a given user."
+
+    def add_arguments(self, parser: ArgumentParser):
+        parser.add_argument("email", help="Email address of user.")
+
+    def handle(self, email: str, **options):
+        """Create an API token for the specified user."""
+        try:
+            user = User.objects.get(email=email)
+        except User.NotFoundError:
+            raise CommandError(f"Could not find user with email {email}")
+
+        try:
+            token = ApiToken.create_token(user)
+        except Exception as exc:
+            raise CommandError(f"Error creating token.") from exc
+
+        self.stdout.write(f"Token created for {email}")
+        self.stdout.write(f"Token: {token}")
