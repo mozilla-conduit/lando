5c5e80bd53439f48e6fa2a96d701cd4afabaffc8
Olivier Mehani <omehani@mozilla.com>
2025-04-09 12:24:30 +1000

    pulse: make module a proper django app

--- src/lando/main/apps.py
+++ src/lando/main/apps.py
@@ -1,6 +1,4 @@
 from django.apps import AppConfig
-from django.conf import settings
-from django.core.checks import Error, Warning, register
 
 from lando.main.sentry import init_sentry
 
@@ -12,20 +10,3 @@ class MainConfig(AppConfig):
     def ready(self):
         """Run initialization tasks."""
         init_sentry()
-
-
-@register(deploy=True)
-def pulse_check(app_configs: list[AppConfig], **kwargs) -> list[Error]:
-    errors = []
-    if settings.PULSE_HOST.startswith("memory"):
-        message = (
-            "PULSE_HOST set to a `memory` location. "
-            + str(settings.PULSE_HOST)
-            + "This should not be the case in non-local deployments. "
-            + settings.ENVIRONMENT
-        )
-        if settings.ENVIRONMENT.is_remote:
-            errors.append(Error(message))
-        else:
-            errors.append(Warning(message))
-    return errors
