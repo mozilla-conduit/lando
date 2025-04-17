68dc37751fb116252d4f651fb1984ba81fc89938
Olivier Mehani <omehani@mozilla.com>
2025-04-17 14:15:50 +1000

    pulse: pulse management commands (bug 1958734) (#284)
    * view_pushlog: show if pushes are notified
    * pulse: Pulse exchanges MUST be of type `topic`
    * pulse_notify: pulse management command (bug 1958734)
    * pulse: move kombu_queue_maker and related fixtures to conftest
    * tests: move pushlog fixtures to base conftest
    * pulse: fix hardcoded routing_key in conftest
    * pulse: don't declare queue on the publisher side
    * pulse_declare: add command to only declare exchange

---
+++ src/lando/pulse/tests/conftest.py
@@ -0,0 +1,73 @@
+from typing import Callable
+
+import pytest
+from kombu import Connection, Consumer, Exchange, Producer, Queue
+
+
+@pytest.fixture()
+def kombu_connection():
+    return Connection("memory://")
+
+
+@pytest.fixture()
+def kombu_exchange():
+    return Exchange("test_exchange", type="topic")
+
+
+@pytest.fixture()
+def make_kombu_queue(kombu_connection, kombu_exchange):
+    def queue_factory(routing_key="routing_key"):
+        queue = Queue("test-queue", kombu_exchange, routing_key=routing_key)
+        queue.maybe_bind(kombu_connection)
+        queue.declare()
+        return queue
+
+    return queue_factory
+
+
+@pytest.fixture
+def kombu_queue_maker(
+    kombu_connection: Connection,
+    kombu_exchange: Exchange,
+    make_kombu_queue: Callable,
+):
+    """Generator yielding first a Producer, then a list of received messages.
+
+    Once the generator has been obtained, it MUST be used to publish some messages
+    prior to requesting the list of received messages. Otherwise, the call will hold for
+    short amount of time, and the returned list will be empty.
+
+    """
+
+    def build_queue(routing_key: str):
+
+        # Producer
+        producer = Producer(
+            channel=kombu_connection.channel(),
+            exchange=kombu_exchange,
+            routing_key=routing_key,
+        )
+        queue = make_kombu_queue(routing_key=routing_key)
+
+        yield producer
+
+        # Consumer
+        messages = []
+
+        def consumer_callback(body, message):
+            messages.append((body, message))
+            message.ack()
+
+        with Consumer(
+            kombu_connection,
+            queues=queue,
+            callbacks=[consumer_callback],
+        ):
+            try:
+                kombu_connection.drain_events(timeout=2)
+            except TimeoutError:
+                pass
+
+        yield messages
+
+    return build_queue
