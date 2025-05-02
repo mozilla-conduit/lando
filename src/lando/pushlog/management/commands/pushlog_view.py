2c7282a1aad7649d38d9a1f874fbb81ad585c3a0
Olivier Mehani <omehani@mozilla.com>
2025-05-03 00:41:03 +1000

    pushlog: show tags in pushlog_view (bug 1962924) (#320)

--- src/lando/pushlog/management/commands/pushlog_view.py
+++ src/lando/pushlog/management/commands/pushlog_view.py
@@ -11,16 +11,20 @@ class Command(BaseCommand):
     name = "pushlog_view"
 
     def add_arguments(self, parser: CommandParser):
-        parser.add_argument("-r", "--repo", required=True)
         parser.add_argument("-l", "--limit", type=int, default=0)
+        parser.add_argument("-r", "--repo", required=True)
         parser.add_argument(
-            "-c", "--commits", default=True, action=argparse.BooleanOptionalAction
+            "-c", "--with-commits", default=True, action=argparse.BooleanOptionalAction
+        )
+        parser.add_argument(
+            "-t", "--with-tags", default=True, action=argparse.BooleanOptionalAction
         )
 
     def handle(self, *args, **options):
-        repo_name = options["repo"]
-        with_commits = options["commits"]
         limit = options["limit"]
+        repo_name = options["repo"]
+        with_commits = options["with_commits"]
+        with_tags = options["with_tags"]
         try:
             repo = Repo.objects.get(name=repo_name)
         except Repo.DoesNotExist:
@@ -34,4 +38,7 @@ class Command(BaseCommand):
             self.stdout.write(f"{push} (notified: {push.notified})")
             if with_commits:
                 for commit in push.commits.order_by("-datetime"):
+                    self.stdout.write(f"  {commit}")
+            if with_tags:
+                for commit in push.tags.all():
                     self.stdout.write(f"  {commit}")
