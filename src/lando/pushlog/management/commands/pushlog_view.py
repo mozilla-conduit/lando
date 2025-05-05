06fdf609f7afafaa6b44de2d0d08342a657aa8f2
Olivier Mehani <omehani@mozilla.com>
2025-05-05 14:58:16 +1000

    pushlog_view: add --push-id argument

--- src/lando/pushlog/management/commands/pushlog_view.py
+++ src/lando/pushlog/management/commands/pushlog_view.py
@@ -12,6 +12,7 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser: CommandParser):
         parser.add_argument("-l", "--limit", type=int, default=0)
+        parser.add_argument("-p", "--push-id", type=int, default=0)
         parser.add_argument("-r", "--repo", required=True)
         parser.add_argument(
             "-c", "--with-commits", default=True, action=argparse.BooleanOptionalAction
@@ -22,6 +23,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         limit = options["limit"]
+        push_id = options["push_id"]
         repo_name = options["repo"]
         with_commits = options["with_commits"]
         with_tags = options["with_tags"]
@@ -30,9 +32,16 @@ class Command(BaseCommand):
         except Repo.DoesNotExist:
             raise CommandError(f"Repository not found: {repo_name}")
 
-        pushes = Push.objects.filter(repo=repo).order_by("-push_id")
-        if limit:
-            pushes = pushes[:limit]
+        if push_id > 0:
+            pushes = Push.objects.filter(repo=repo, push_id=push_id)
+        else:
+            pushes = Push.objects.filter(repo=repo).order_by("-push_id")
+            if limit:
+                pushes = pushes[:limit]
+
+        if not pushes:
+            push_id_info = f" (push_id {push_id})" if push_id > 0 else ""
+            raise CommandError(f"No push found for {repo_name}{push_id_info}")
 
         for push in pushes:
             self.stdout.write(f"{push} (notified: {push.notified})")
