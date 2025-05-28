8b0701457619332edbc0b40c6a6bcdf60474f619
Olivier Mehani <omehani@mozilla.com>
2025-05-28 11:06:08 +1000

    admin: improvements to Pushlong and Job views (bug 1955972, bug 1965977) (#321)
    * Pushlog
    * Revision
    * LandingJob 
    * Automation API Job and Actions

--- src/lando/pushlog/admin.py
+++ src/lando/pushlog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+from lando.main.admin import ReadOnlyInline
 from lando.pushlog.models import (
     Commit,
     File,
@@ -15,12 +16,42 @@ class PushLogAdmin(admin.ModelAdmin):
         """Forbid addition of any pushlog object from the admin interface."""
         return False
 
+    def has_change_permission(self, request, obj=None) -> bool:  # noqa: ANN001
+        """Forbid change of any pushlog object from the admin interface."""
+        return False
+
     def has_delete_permission(self, request, obj=None) -> bool:  # noqa: ANN001
         """Forbid deletion of any pushlog object from the admin interface."""
         return False
 
 
+class PushCommitInline(ReadOnlyInline):
+    # https://docs.djangoproject.com/en/5.0/ref/contrib/admin/#working-with-many-to-many-models
+    model = Push.commits.through
+
+    # Used by ReadOnlyInline._field_getter_factory
+    _target_object = "commit"
+
+    readonly_fields = ["hash_short", "desc", "author"]
+
+    def hash_short(self, instance: Commit) -> str:
+        return instance.commit.hash[:8]
+
+    def desc(self, instance: Commit) -> str:
+        return instance.commit.desc.splitlines()[0]
+
+
+class PushTagInline(ReadOnlyInline):
+    model = Push.tags.through
+
+    # Used by ReadOnlyInline._field_getter_factory
+    _target_object = "tag"
+
+    readonly_fields = ["name", "commit"]
+
+
 class PushAdmin(PushLogAdmin):
+    model = Push
     readonly_fields = (
         "push_id",
         "notified",
@@ -29,14 +60,69 @@ class PushAdmin(PushLogAdmin):
         "branch",
         "datetime",
         "user",
-        "commits",
-        "tags",
     )
-    list_display = ["push_id", "notified", "repo__name", "branch", "datetime", "user"]
+    inlines = [PushCommitInline, PushTagInline]
+    exclude = ["commits", "tags"]
+    list_display = [
+        "push_id",
+        "notified",
+        "repo__name",
+        "branch",
+        "commit_summary",
+        "tag_summary",
+        "datetime",
+        "user",
+    ]
     list_filter = ["repo", "branch", "user", "datetime"]
+
+    def commit_summary(self, instance: Push) -> str:
+        """Return a summary of commits present in a Push.
+
+        The summary is the hash of the last commit, and a count of all the other
+        commits present in the Push.
+        """
+        last_commit = instance.commits.order_by("-id").last()
+        ncommits = instance.commits.count()
+        summary = "(no commit)"
+        if last_commit:
+            summary = last_commit.hash[:8]
+            if (ncommits := ncommits - 1) > 0:
+                summary = f"{summary} and {ncommits} commit{'s' if ncommits else ''}"
+
+        return summary
+
+    def tag_summary(self, instance: Push) -> str:
+        """Return a summary of tags present in a Push.
+
+        The summary is the hash of the last tag, and a count of all the other
+        tags present in the Push.
+        """
+        last_tag = instance.tags.order_by("-id").last()
+        ntags = instance.tags.count()
+        summary = "(no tag)"
+        if last_tag:
+            summary = f"{last_tag.name} to {last_tag.commit.hash}"
+            if (ntags := ntags - 1) > 0:
+                summary = f"{summary} and {ntags} tag{'s' if ntags else ''}"
+
+        return summary
+
+
+class CommitPushInline(ReadOnlyInline):
+    model = Push.commits.through
+    _target_object = "push"
+
+    readonly_fields = [
+        "push_id",
+        "repo",
+        "branch",
+        "datetime",
+        "user",
+    ]
 
 
 class CommitAdmin(PushLogAdmin):
+    model = Commit
     readonly_fields = (
         "repo",
         "hash",
@@ -47,8 +133,15 @@ class CommitAdmin(PushLogAdmin):
         "_files",
         "_parents",
     )
-    list_display = ["hash", "repo__name", "datetime", "author"]
+    list_display = ["hash_short", "repo__name", "desc_first", "datetime", "author"]
     list_filter = ["repo", "author", "datetime"]
+    inlines = [CommitPushInline]
+
+    def hash_short(self, instance: Commit) -> str:
+        return instance.hash[:8]
+
+    def desc_first(self, instance: Commit) -> str:
+        return instance.desc.splitlines()[0]
 
 
 class FileAdmin(PushLogAdmin):
@@ -60,6 +153,11 @@ class FileAdmin(PushLogAdmin):
     list_filter = ["repo"]
 
 
+class TagPushInline(CommitPushInline):
+    model = Push.tags.through
+    _target_object = "push"
+
+
 class TagAdmin(PushLogAdmin):
     readonly_fields = (
         "repo",
@@ -68,6 +166,7 @@ class TagAdmin(PushLogAdmin):
     )
     list_display = ["name", "commit", "repo__name"]
     list_filter = ["repo"]
+    inlines = [TagPushInline]
 
 
 admin.site.register(Push, PushAdmin)
