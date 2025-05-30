5a96cef523412082c851d9450dccd5567c5095a7
Olivier Mehani <omehani@mozilla.com>
2025-05-30 22:26:28 +1000

    settings: bump DATA_UPLOAD_MAX_MEMORY_SIZE (bug 1964604) (#342)

--- src/lando/settings.py
+++ src/lando/settings.py
@@ -48,6 +48,9 @@ SECURE_SSL_REDIRECT = False
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
+# Set the default upload size to 10x the default value (2.5MiB).
+DATA_UPLOAD_MAX_MEMORY_SIZE = int(os.getenv("DATA_UPLOAD_MAX_MEMORY_SIZE", "26214400"))
+
 # Application definition
 
 INSTALLED_APPS = [
