dea792ad53f5e1d65d97015015083a3f7b5fe60f
Zeid <2043828+zzzeid@users.noreply.github.com>
2025-05-30 14:48:26 -0400

    settings: double DATA_UPLOAD_MAX_MEMORY_SIZE default value (bug 1964604) (#343)

--- src/lando/settings.py
+++ src/lando/settings.py
@@ -48,8 +48,8 @@ SECURE_SSL_REDIRECT = False
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
-# Set the default upload size to 10x the default value (2.5MiB).
-DATA_UPLOAD_MAX_MEMORY_SIZE = int(os.getenv("DATA_UPLOAD_MAX_MEMORY_SIZE", "26214400"))
+# Set the default upload memory size to 20x the default value (2.5MiB).
+DATA_UPLOAD_MAX_MEMORY_SIZE = int(os.getenv("DATA_UPLOAD_MAX_MEMORY_SIZE", "52428800"))
 
 # Application definition
 
