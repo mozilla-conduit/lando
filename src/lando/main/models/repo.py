5c946855f78c790f1c016fe93b8fda7e41300346
Zeid <zeid@mozilla.com>
2025-07-03 14:53:16 -0400

    code review feedback

--- src/lando/main/models/repo.py
+++ src/lando/main/models/repo.py
@@ -223,8 +223,7 @@ class Repo(BaseModel):
         if not self.scm_type:
             self.scm_type = self._find_supporting_scm(self.pull_path)
 
-        if self.url.endswith("/"):
-            self.url = self.url[:-1]
+        self.url = self.url.removesuffix("/")
 
         if self.is_github and not self.url.endswith(".git"):
             self.url += ".git"
@@ -239,23 +238,28 @@ class Repo(BaseModel):
     @property
     def is_github(self) -> bool:
         """Return `True` if repo URL is a GitHub URL."""
-        if not self.is_git:
-            return False
-        if self.parsed_url.hostname:
-            return self.parsed_url.hostname.endswith("github.com")
-        return False
+        return (
+            self.is_git
+            and self.parsed_url.hostname
+            and self.parsed_url.hostname.endswith("github.com")
+        )
 
     @property
     def normalized_url(self) -> str:
+        """Return GitHub URL without `.git` suffix, or the original URL.
+
+        For repos hosted on GitHub, remove the `.git` suffix from the URL. This allows the URL
+        to be used as a base URL for other paths, e.g. specific commits. For non-GitHub repos,
+        return the original URL.
+        """
         if self.is_github:
             return self._github_repo_url
         return self.url
 
     @property
-    def _github_repo_url(self) -> str:
+    def _github_repo_url(self) -> Optional[str]:
         if self.is_github:
             return self.url.removesuffix(".git")
-        return ""
 
     @property
     def _github_repo_name(self) -> str:
