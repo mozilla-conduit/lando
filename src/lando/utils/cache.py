34812229c035ff68370c11cf16aeef0c2f8e23be
Connor Sheehan <cosheehan@mozilla.com>
2025-08-08 15:36:48 -0400

    fix docstring

--- src/lando/utils/cache.py
+++ src/lando/utils/cache.py
@@ -13,14 +13,18 @@ T = TypeVar("T")
 def django_cache_method(
     key_fn: Callable[..., str],
 ) -> Callable[[Callable[..., T]], Callable[..., T]]:
-    """Decorator factory that caches the result of a method using the provided key function."""
+    """Cache the method result using the key function.
+
+    Decorator factory that caches the result of a method using
+    the provided key function.
+    """
 
     def decorator(func: Callable[..., T]) -> Callable[..., T]:
         @functools.wraps(func)
         def wrapper(*args, **kwargs) -> T:
             key = key_fn(*args, **kwargs)
 
-            if cache.has_key(key):  # noqa: W601
+            if cache.has_key(key):
                 return cache.get(key)
 
             result = func(*args, **kwargs)
