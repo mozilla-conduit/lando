3ac4ed5da5b69fcd462178424173e1fa298d18a1
Olivier Mehani <omehani@mozilla.com>
2025-08-14 12:51:59 +1000

    fixup! try_api: userinfo test stub

--- src/lando/try_api/api.py
+++ src/lando/try_api/api.py
@@ -1,5 +1,7 @@
 import logging
 
+from django.conf import settings
+from django.contrib.auth.models import User
 from django.core.handlers.wsgi import WSGIRequest
 from django.http import JsonResponse
 from ninja import NinjaAPI
@@ -12,7 +14,10 @@ logger = logging.getLogger(__name__)
 
 
 class GlobalAuth(HttpBearer):
+    """Bearer token-based authenticator delegating verification to the OIDC backend."""
+
     def authenticate(self, request: WSGIRequest, token: str) -> User:
+        """Forward the authenticate request to the LandoOIDCAuthenticationBackend."""
         # The token is extracted in the LandoOIDCAuthenticationBackend, so we don't need
         # to pass it. But we need to inherit from HttpBearer for Auth to work.
         oidc_auth = LandoOIDCAuthenticationBackend()
@@ -24,6 +29,9 @@ api = NinjaAPI(urls_namespace="try", auth=GlobalAuth())
 
 @api.get("/__userinfo__")
 def userinfo(request: WSGIRequest) -> JsonResponse:
-    if not settings.ENVIRONMENT.is_lower():
+    """Test endpoint to check token verification.
+
+    Only available in non-prod environments."""
+    if not settings.ENVIRONMENT.is_lower:
         raise HttpError(404, "Not Found")
-    return JsonResponse({"token": str(request.auth)})
+    return JsonResponse({"user_id": str(request.auth)})
