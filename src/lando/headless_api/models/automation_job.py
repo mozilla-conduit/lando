197faf6254a494645ad4a0e4bceb3e9b80820a67
Olivier Mehani <omehani@mozilla.com>
2025-08-22 11:35:39 +1000

    fixup! jobs: factorise common job logic into BaseJob (bug 1983208)

--- src/lando/headless_api/models/automation_job.py
+++ src/lando/headless_api/models/automation_job.py
@@ -1,6 +1,6 @@
 from contextlib import contextmanager
 from datetime import datetime
-from typing import Any, Iterable, Optional
+from typing import Any
 
 from django.conf import settings
 from django.db import models
