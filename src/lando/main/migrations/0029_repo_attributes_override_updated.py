ee397aab0724828e196ac71397a32aa0c1fa03b4
Olivier Mehani <omehani@mozilla.com>
2025-08-25 18:29:07 +1000

    avoid migration conflict

--- src/lando/main/migrations/0029_repo_attributes_override_updated.py
+++ src/lando/main/migrations/0029_repo_attributes_override_updated.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("main", "0027_alter_landingjob_status_worker_type"),
+        ("main", "0028_upliftquestionnaireresponse_upliftrevision"),
     ]
 
     operations = [
