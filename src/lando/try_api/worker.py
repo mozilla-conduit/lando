bcb2f188eacbe1aa4f74997a8ea7f253e1e21e35
Olivier Mehani <omehani@mozilla.com>
2025-09-11 17:36:40 +1000

    fixup! fixup! try: stub worker (bug 1979249)

--- src/lando/try_api/worker.py
+++ src/lando/try_api/worker.py
@@ -1,17 +1,16 @@
 from typing_extensions import override
 
 from lando.api.legacy.workers.base import Worker
-from lando.headless_api.models.automation_job import AutomationJob
 from lando.main.models import (
-    BaseJob,
     WorkerType,
 )
+from lando.try_api.models.job import TryJob
 
 
 class TryWorker(Worker):
-    job_type = AutomationJob
+    job_type = TryJob
     worker_type = WorkerType.TRY
 
     @override
-    def run_job(self, job: BaseJob) -> bool:
+    def run_job(self, job: TryJob) -> bool:
         raise NotImplementedError()
