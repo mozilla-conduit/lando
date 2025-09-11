f52876e110d226ae6170fd381918c4342a3425b2
Olivier Mehani <omehani@mozilla.com>
2025-09-11 17:11:56 +1000

    try_api: add model

---
+++ src/lando/try_api/admin.py
@@ -0,0 +1,30 @@
+
+from django.contrib import admin
+from django.core.handlers.wsgi import WSGIRequest
+
+from lando.main.admin import JobAdmin, ReadOnlyInline
+from lando.try_api.models.job import TryAction, TryJob
+
+class TryActionJobInline(ReadOnlyInline):
+    model = TryAction
+    _target_object = "actions"
+    readonly_fields = ("action_type", "data", "order")
+
+    def has_add_permission(
+        self, request: WSGIRequest, obj: TryAction | None = None
+    ) -> bool:
+        """Forbid addition of any action object from the inline interface."""
+        return False
+
+    def has_delete_permission(
+        self, request: WSGIRequest, obj: TryAction | None = None
+    ) -> bool:
+        """Forbid deletion of any action object from the inline interface."""
+        return False
+
+class TryJobAdmin(JobAdmin):
+    model = TryJob
+    inlines = (TryActionJobInline,)
+
+admin.site.register(TryJob, TryJobAdmin)
+admin.site.register(TryAction, admin.ModelAdmin)  # noqa: F821
---
+++ src/lando/try_api/models/job.py
@@ -0,0 +1,28 @@
+
+from django.db import models
+
+from lando.main.models import (
+    BaseJob,
+    BaseModel,
+)
+
+
+class TryJob(BaseJob):
+    """Represent a Try job."""
+    type = "Try"
+
+    target_commit_hash = models.TextField(blank=True, default="")
+
+class TryAction(BaseModel):
+    job_id = models.ForeignKey(
+        TryJob, on_delete=models.CASCADE, related_name="actions"
+    )
+
+    # Data for each individual action. Data in these fields should be
+    # parsable into the appropriate Pydantic schema.
+    data = models.JSONField()
+
+    order = models.PositiveIntegerField()
+
+    class Meta:
+        ordering = ["order"]
