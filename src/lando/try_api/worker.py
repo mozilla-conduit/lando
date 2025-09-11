1cca299a6949ecb011061214e2a9369ec4d037db
Olivier Mehani <omehani@mozilla.com>
2025-09-11 12:49:55 +1000

    try: stub worker (bug 1979249)

---
+++ src/lando/try_api/worker.py
@@ -0,0 +1,17 @@
+from typing_extensions import override
+
+from lando.api.legacy.workers.base import Worker
+from lando.main.models import (
+    WorkerType,
+)
+from lando.main.models.jobs import TemporaryFailureException
+from lando.try_api.models.job import TryJob
+
+
+class TryWorker(Worker):
+    job_type = TryJob
+    worker_type = WorkerType.TRY
+
+    @override
+    def run_job(self, job: TryJob) -> bool:
+        raise TemporaryFailureException("Bled")
