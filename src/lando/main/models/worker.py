2135d2808cf03db9338c54a6bc8e441372395341
Olivier Mehani <omehani@mozilla.com>
2025-09-11 12:45:21 +1000

    models: add Try WorkerType (bug 1979249)

--- src/lando/main/models/worker.py
+++ src/lando/main/models/worker.py
@@ -16,6 +16,7 @@ class WorkerType(models.TextChoices):
     LANDING = "LANDING", gettext_lazy("Landing worker")
     AUTOMATION = "AUTOMATION", gettext_lazy("Automation worker")
     UPLIFT = "UPLIFT", gettext_lazy("Uplift worker")
+    TRY = "TRY", gettext_lazy("Try worker")
 
 
 class Worker(BaseModel):
