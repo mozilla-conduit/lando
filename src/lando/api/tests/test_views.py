22e26b504f3370654fa6b23085b766d46c0c1161
Zeid <zeid@mozilla.com>
2025-10-06 14:30:31 -0400

    code review feedback

--- src/lando/api/tests/test_views.py
+++ src/lando/api/tests/test_views.py
@@ -43,3 +43,22 @@ def test__views__git2hgCommitMapView_unknown_commit(commit_maps, client, monkeyp
     assert response.json().get("error") == "No commits found"
     assert mock_catch_up.call_count == 1
     assert mock_catch_up.call_args[0] == ("git_repo",)
+
+
+@pytest.mark.django_db(transaction=True)
+def test__views__git2hgCommitMapView_multiple_commits(commit_maps, client, monkeypatch):
+    mock_catch_up = mock.MagicMock()
+    monkeypatch.setattr("lando.api.views.CommitMap.catch_up", mock_catch_up)
+    response = client.get("/api/git2hg/git_repo/aaaaaaa")
+    assert response.status_code == 400
+    assert response.json().get("error") == "Multiple commits found"
+
+
+@pytest.mark.django_db(transaction=True)
+def test__views__git2hgCommitMapView_short_hash(commit_maps, client, monkeypatch):
+    mock_catch_up = mock.MagicMock()
+    monkeypatch.setattr("lando.api.views.CommitMap.catch_up", mock_catch_up)
+    commit_map = commit_maps[2]
+    response = client.get("/api/git2hg/git_repo/ccccccc")
+    assert response.status_code == 200
+    assert response.json() == commit_map.serialize()
