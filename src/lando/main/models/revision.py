5be9bfbc29a362ef9dd5af30a900c5f4833bf780
Olivier Mehani <omehani@mozilla.com>
2025-10-07 11:37:01 +1100

    fixup! Revert "fixup! Revert "revisions: let the model determine the URL""

--- src/lando/main/models/revision.py
+++ src/lando/main/models/revision.py
@@ -91,7 +91,6 @@ class Revision(BaseModel):
         if job := self.landing_jobs.first():
             return f"{settings.SITE_URL}/landings/{job.id}#r{self.id}"
 
-        # This is not a functional URL, but this is last resort.
         return f"No URL for Revision #{self.id}"
 
     @property
