dc2169cb2e965f71571bb482e958ba0555c63459
Olivier Mehani <omehani@mozilla.com>
2025-10-14 13:01:27 +1100

    github_warnings: Reimplement Phabricator warnings (bug 1994359)

---
+++ src/lando/utils/github_warnings.py
@@ -0,0 +1,141 @@
+from abc import ABC, abstractmethod
+
+from typing_extensions import override
+
+from lando.utils.github import GitHubAPIClient, PullRequest
+
+
+class PullRequestWarning(ABC):
+
+    @classmethod
+    @abstractmethod
+    def run(cls, client: GitHubAPIClient, pull_request: PullRequest) -> str | None:
+        """Inspect the PR for on issue, and return a warning string if present."""
+
+
+class PullRequestBlockingReviewsWarning(PullRequestWarning):
+    """Has a review intended to block landing."""
+
+    @override
+    @classmethod
+    def run(cls, client: GitHubAPIClient, pull_request: PullRequest) -> str | None:
+        raise NotImplementedError
+
+
+class PullRequestPreviouslyLandedWarning(PullRequestWarning):
+    """Has previously landed."""
+
+    @override
+    @classmethod
+    def run(cls, client: GitHubAPIClient, pull_request: PullRequest) -> str | None:
+        return parse_multiline_adjlist.__doc__
+
+
+class PullRequestNotAcceptedWarning(PullRequestWarning):
+    """Is not Accepted."""
+
+    @override
+    @staticmethod
+    def run(client: GitHubAPIClient, pull_request: PullRequest) -> str | None:
+        raise NotImplementedError
+
+
+class PullRequestReviewsNotCurrentWarning(PullRequestWarning):
+    """No reviewer has accepted the current diff."""
+
+    @override
+    @staticmethod
+    def run(client: GitHubAPIClient, pull_request: PullRequest) -> str | None:
+        raise NotImplementedError
+
+
+class PullRequestSecureRevisionWarning(PullRequestWarning):
+    """Is a secure pull request and should follow the Security Bug Approval Process."""
+
+    @override
+    @classmethod
+    def run(cls, client: GitHubAPIClient, pull_request: PullRequest) -> str | None:
+        raise NotImplementedError
+
+
+class PullRequestMissingTestingTagWarning(PullRequestWarning):
+    """Pull request is missing a Testing Policy Project Tag."""
+
+    @override
+    @classmethod
+    def run(cls, client: GitHubAPIClient, pull_request: PullRequest) -> str | None:
+        raise NotImplementedError
+
+
+class PullRequestDiffWarning(PullRequestWarning):
+    """Pull request has a diff warning."""
+
+    @override
+    @classmethod
+    def run(cls, client: GitHubAPIClient, pull_request: PullRequest) -> str | None:
+        raise NotImplementedError
+
+
+class PullRequestWIPWarning(PullRequestWarning):
+    """Pull request is marked as WIP."""
+
+    @override
+    @classmethod
+    def run(cls, client: GitHubAPIClient, pull_request: PullRequest) -> str | None:
+        raise NotImplementedError
+
+
+class PullRequestCodeFreezeWarning(PullRequestWarning):
+    """Repository is under a soft code freeze."""
+
+    @override
+    @classmethod
+    def run(cls, client: GitHubAPIClient, pull_request: PullRequest) -> str | None:
+        raise NotImplementedError
+
+
+class PullRequestUnresolvedCommentsWarning(PullRequestWarning):
+    """Pull request has unresolved comments."""
+
+    @override
+    @classmethod
+    def run(cls, client: GitHubAPIClient, pull_request: PullRequest) -> str | None:
+        raise NotImplementedError
+
+
+class PullRequestMultipleAuthorsWarning(PullRequestWarning):
+    """Pull request has multiple authors."""
+
+    @override
+    @classmethod
+    def run(cls, client: GitHubAPIClient, pull_request: PullRequest) -> str | None:
+        raise NotImplementedError
+
+
+class PullRequestWarningChecks:
+    """Utility class to check a GitHub pull request for a given list of issues."""
+
+    _client: GitHubAPIClient
+
+    def __init__(self, client: GitHubAPIClient):
+        self._client = client
+
+    def run(
+        self, warnings_list: list[PullRequestWarning], pull_request: PullRequest
+    ) -> list[str]:
+        warnings = []
+
+        for wrn in warnings_list:
+            try:
+                if outcome := wrn.run(self._client, pull_request):
+                    warnings.append(outcome)
+            except NotImplementedError:
+                warnings.append(f"{wrn.__name__} is not implemented")
+
+            except Exception as exc:
+                warnings.append(f"{wrn.__name__} failed to run with error: {exc}")
+
+        return warnings
+
+
+ALL_PULLREQUEST_WARNINGS = PullRequestWarning.__subclasses__()
