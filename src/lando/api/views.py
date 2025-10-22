ce54380782f6e741bcab05e1be206052687a2070
Zeid <2043828+zzzeid@users.noreply.github.com>
2025-10-22 09:42:02 -0400

    github: add pull request endpoint (bug 1991125) (#582)
    - add `PullRequestAPIView.get` endpoint
    - add placeholder for `LandingJobAPIView.post` endpoint
    - add `serialize` method to `PullRequest` class
    - update urls with above

--- src/lando/api/views.py
+++ src/lando/api/views.py
@@ -9,13 +9,20 @@ from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 
-from lando.main.models import CommitMap
+from lando.main.models import CommitMap, Repo
 from lando.main.models.revision import DiffWarning, DiffWarningStatus
 from lando.main.scm import (
     SCM_TYPE_GIT,
     SCM_TYPE_HG,
 )
+from lando.utils.github import GitHubAPIClient, PullRequest
 from lando.utils.phabricator import get_phabricator_client
+
+
+class APIView(View):
+    """A base class for API views."""
+
+    pass
 
 
 def phabricator_api_key_required(func: callable) -> Callable:
@@ -148,3 +155,22 @@ class hg2gitCommitMapView(CommitMapBaseView):
     """Return corresponding CommitMap given an hg hash."""
 
     scm = SCM_TYPE_HG
+
+
+class PullRequestAPIView(APIView):
+    """Handle pull requests in the API."""
+
+    def get(self, request: WSGIRequest, repo_name: str, number: int) -> JsonResponse:
+        """Return a serialized JSON representation of a pull request."""
+        target_repo = Repo.objects.get(name=repo_name)
+        client = GitHubAPIClient(target_repo)
+        pull_request = PullRequest(client.get_pull_request(number))
+        return JsonResponse(pull_request.serialize(), status=200)
+
+
+class LandingJobAPIView(View):
+    """Handle landing jobs in the API."""
+
+    def post(self, request: WSGIRequest, *args, **kwargs):
+        """Placeholder for creating new landing jobs."""
+        pass
