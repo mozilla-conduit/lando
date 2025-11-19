0d83d8821923a56ef39eba2f1883df924e3c40ca
Zeid <zeid@mozilla.com>
2025-11-19 15:31:59 -0500

    code review feedback

--- src/lando/static_src/legacy/js/components/Stack.js
+++ src/lando/static_src/legacy/js/components/Stack.js
@@ -59,34 +59,28 @@ $.fn.stack = function() {
                 } else {
                     fetch(`/api/pulls/${repo_name}/${pull_number}/checks`, {
                         method: 'GET',
-                        headers: {
-                            'Accept': 'application/json',
-                            'Content-Type': 'application/json',
-                            'X-CSRFToken': csrf_token
-                        },
                     }).then(async response => {
                         if (response.status == 200) {
                             var result = await response.json();
                             var blockers = result.blockers;
                             var warnings = result.warnings;
-                            if (blockers.length !== 0) {
-                                pull_request_button.prop("disabled", true);
-                                pull_request_button.removeClass("is-loading").addClass("is-danger");
-                                pull_request_button.html("Landing is blocked");
-                            }
-                            $("#blockers").html("");
 
                             if (blockers.length === 0) {
                                 $("#blockers").html("None found.");
-                            }
-
-                            for (var blocker of blockers) {
-                                $("#blockers").append(`<li>${blocker}</li>`);
+                                pull_request_button.prop("disabled", false);
+                                pull_request_button.removeClass("is-loading").addClass("is-success");;
+                                pull_request_button.html("Request landing");
+                            } else {
+                                $("#blockers").html("");
+                                pull_request_button.prop("disabled", true);
+                                pull_request_button.removeClass("is-loading").addClass("is-danger");
+                                pull_request_button.html("Landing is blocked");
+                                for (var blocker of blockers) {
+                                    $("#blockers").append(`<li>${blocker}</li>`);
+                                }
                             }
                         } else {
-                            pull_request_button.prop("disabled", false);
-                            pull_request_button.removeClass("is-loading").addClass("is-success");;
-                            pull_request_button.html("Request landing");
+                            // TODO: handle this case. See bug 1996000.
                         }
                     });
                 }
