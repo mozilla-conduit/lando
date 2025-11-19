6ba07cf304909aa47f4a9ddeab3bee8f54ab6048
Olivier Mehani <omehani@mozilla.com>
2025-11-19 12:11:24 +1100

    squash! fixup! fixup! github: add PullRequestPatchHelper
    keep reference to PullRequest and forward author call

--- src/lando/utils/github.py
+++ src/lando/utils/github.py
@@ -316,7 +316,7 @@ class PullRequest:
         return self.client.get_pull_request_commits(self.number)
 
     @property
-    def author(self) -> str:
+    def author(self) -> tuple[str | None, str | None]:
         return self._select_commit_author(self.commits)
 
     def serialize(self) -> dict[str, str]:
@@ -364,17 +364,20 @@ class PullRequestPatchHelper(PatchHelper):
 
     _author_name: str
     _author_email: str
+    _pr: PullRequest
 
     def __init__(self, pr: PullRequest):
         super().__init__()
 
+        self._pr = pr
+
         self._diff = pr.diff
 
-        self._author_name, self._author_email = pr.author
+        author_name, author_email = pr.author
 
         self.headers = {
             "date": self._get_timestamp_from_github_timestamp(pr.updated_at),
-            "from": f"{self._author_name} <{self._author_email}>",
+            "from": f"{author_name} <{author_email}>",
             "subject": pr.title,
         }
 
@@ -413,7 +416,7 @@ class PullRequestPatchHelper(PatchHelper):
     @override
     def parse_author_information(self) -> tuple[str, str]:
         """Return the author name and email from the patch."""
-        return (self._author_name, self._author_email)
+        return self._pr.author
 
     @override
     def get_timestamp(self) -> str:
