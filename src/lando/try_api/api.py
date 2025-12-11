e882d3286b877fa51a8ffd848750eb503d39a53a
Olivier Mehani <omehani@mozilla.com>
2025-12-11 16:15:08 +1100

    utils: move auth endpoints from try_api

--- src/lando/try_api/api.py
+++ src/lando/try_api/api.py
@@ -3,12 +3,10 @@ import io
 import logging
 from typing import Annotated
 
-from django.conf import settings
 from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponsePermanentRedirect, JsonResponse
 from django.shortcuts import redirect
 from ninja import NinjaAPI, Schema
-from ninja.errors import HttpError
 from pydantic import Field, StringConstraints
 
 from lando.main.auth import require_authenticated_user, require_permission
@@ -28,14 +26,7 @@ logger = logging.getLogger(__name__)
 api = NinjaAPI(urls_namespace="try", auth=AccessTokenAuth())
 
 
-@api.get("/__userinfo__")
-def userinfo(request: WSGIRequest) -> JsonResponse:
-    """Test endpoint to check token verification.
 
-    Only available in non-prod environments."""
-    if not settings.ENVIRONMENT.is_lower:
-        raise HttpError(404, "Not Found")
-    return JsonResponse({"user_id": str(request.auth)})
 
 
 Base64Patch = Annotated[
--- src/lando/urls.py
+++ src/lando/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path, re_path, reverse_lazy
 from django.views.generic.base import RedirectView
@@ -43,12 +44,19 @@ from lando.try_api.api import (
 )
 from lando.ui import jobs, pull_requests
 from lando.ui.legacy import pages, revisions, user_settings
+from lando.utils.auth import api as auth_api
 
 urlpatterns = [
     path("", include("lando.dockerflow.urls", "dockerflow")),
     path("admin/", admin.site.urls),
     path("oidc/", include("mozilla_django_oidc.urls")),
 ]
+
+if settings.ENVIRONMENT.is_lower:
+    urlpatterns += [
+        path("auth/", auth_api.urls),
+    ]
+
 
 urlpatterns += [
     path("", pages.IndexView.as_view()),
