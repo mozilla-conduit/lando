1d78695fbb9dcab068ce336692800572f1ffab0f
Olivier Mehani <omehani@mozilla.com>
2025-12-11 16:15:08 +1100

    utils: move auth endpoints from try_api (bug 2005993)

--- src/lando/try_api/api.py
+++ src/lando/try_api/api.py
@@ -1,23 +1,9 @@
 import logging
 
-from django.conf import settings
-from django.core.handlers.wsgi import WSGIRequest
-from django.http import JsonResponse
 from ninja import NinjaAPI
-from ninja.errors import HttpError
 
 from lando.utils.auth import AccessTokenAuth
 
 logger = logging.getLogger(__name__)
 
 api = NinjaAPI(urls_namespace="try", auth=AccessTokenAuth())
-
-
-@api.get("/__userinfo__")
-def userinfo(request: WSGIRequest) -> JsonResponse:
-    """Test endpoint to check token verification.
-
-    Only available in non-prod environments."""
-    if not settings.ENVIRONMENT.is_lower:
-        raise HttpError(404, "Not Found")
-    return JsonResponse({"user_id": str(request.auth)})
