ecf4a005d8b07ab3df2db5e74151b777c128bfe2
Olivier Mehani <omehani@mozilla.com>
2026-01-07 18:33:41 +1100

    utils.exception: add support for ForbiddenProblemException (bug 2008894)

--- src/lando/utils/exceptions.py
+++ src/lando/utils/exceptions.py
@@ -1,4 +1,7 @@
+from django.core.exceptions import PermissionDenied
+from django.core.handlers.wsgi import WSGIRequest
 from ninja import Schema
+from ninja.responses import Response
 
 
 class ProblemDetail(Schema):
@@ -15,6 +18,10 @@ class ProblemDetail(Schema):
                 case 400:
                     kwargs["type"] = (
                         "https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/400"
+                    )
+                case 403:
+                    kwargs["type"] = (
+                        "https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/403"
                     )
                 case 404:
                     kwargs["type"] = (
@@ -63,6 +70,22 @@ class BadRequestProblemException(ProblemException):
         )
 
 
+class ForbiddenProblemException(ProblemException):
+    """`ProblemException` subclass for `403 Forbidden` errors."""
+
+    def __init__(self, title: str, detail: str):
+        super().__init__(
+            title=title,
+            detail=detail,
+            status=403,
+        )
+
+    @staticmethod
+    def from_permission_denied(exc: PermissionDenied) -> "ForbiddenProblemException":
+        """Convert a Django PermissionDenied to a ForbiddenProblemException."""
+        return ForbiddenProblemException(title="Forbidden", detail=str(exc))
+
+
 class NotFoundProblemException(ProblemException):
     """`ProblemException` subclass for `404 Not Found` errors."""
 
@@ -73,12 +96,13 @@ class NotFoundProblemException(ProblemException):
             status=404,
         )
 
+
 def problem_exception_handler(request: WSGIRequest, exc: ProblemException) -> Response:
     """Convert a thrown `ProblemException` to a response.
 
     To install for your API, use the following syntax:
 
-        api = NinjaAPI(auth=None, urls_namespace="treestatus-api")
+        api = NinjaAPI(...)
         api.exception_handler(ProblemException)(problem_exception_handler)
     """
     return Response(
