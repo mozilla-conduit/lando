05f41cac66a5a93c7bd2f9a043d8d24669b8e522
Olivier Mehani <omehani@mozilla.com>
2026-01-30 12:35:02 +1100

    test: ensure that AccessTokenLandoOIDCAuthenticationBackend creates users correctly

--- src/lando/utils/tests/test_ninja_auth.py
+++ src/lando/utils/tests/test_ninja_auth.py
@@ -28,6 +28,59 @@ def test_authentication_valid_token(
 
 
 @pytest.mark.django_db()
+@patch("lando.main.auth.AccessTokenLandoOIDCAuthenticationBackend.get_userinfo")
+def test_authentication_valid_token_non_existent_user(
+    mock_get_userinfo: MagicMock, client: Client
+):
+    """Checks that users are created correctly when first authenticating with a token."""
+    mock_get_userinfo.return_value = {
+        "dn": None,
+        "email": "api-user@example.com",
+        "email_aliases": None,
+        "email_verified": True,
+        "family_name": "user",
+        "given_name": "api",
+        "https://sso.mozilla.com/claim/groups": [
+            "active_scm_level_1",
+            "all_scm_level_1",
+            "all_scm_level_3",
+            "everyone",
+        ],
+        "name": "api-user",
+        "nickname": "api-user",
+        "organizationUnits": None,
+        "picture": "https://example.com/api-user.png",
+        "sub": "ad|Mozilla-LDAP|api-user",
+        "updated_at": "2026-01-23T00:55:19.962Z",
+    }
+
+    user_count = len(User.objects.all())
+
+    response = client.get(
+        "/auth/__userinfo__",
+        # The value of the token doesn't actually matter, as the output is controlled by
+        # the authenticator function, which we mock to return a User.
+        headers={"AuThOrIzAtIoN": "bEaReR valid_token"},
+    )
+
+    assert mock_get_userinfo.called, "get_userinfo should be called"
+    assert response.status_code == 200, "Valid token should result in 200"
+
+    assert len(User.objects.all()) == user_count + 1, "No new user created"
+
+    user = User.objects.last()
+
+    assert hasattr(user, "profile"), "Token-created user is missing its Profile"
+
+    assert user.has_perm(
+        "main.scm_level_1"
+    ), "User is missing scm_level_1 permission, despite it being active"
+    assert not user.has_perm(
+        "main.scm_level_3"
+    ), "User has scm_level_3 permission, despite it being inactive"
+
+
+@pytest.mark.django_db()
 def test_authentication_no_token(client: Client, ninja_api_client: Callable):
     response = ninja_api_client(api).get("/__userinfo__")
     assert response.status_code == 401, "Missing token should result in 401"
