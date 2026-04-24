f40966ced825b1e8be4c4b20f113d3c0e297bb90
Olivier Mehani <omehani@mozilla.com>
2026-04-24 16:02:29 +1000

    ui: show recent jobs on homepage when logged in (bug 1652787)
    This requires a tiny tweak to the TimeLine CSS to force `margin-bottom`
    to 0 in `-itemDetails` (otherwise it gets overriden by Bulma's
    `content`).

--- src/lando/ui/legacy/pages.py
+++ src/lando/ui/legacy/pages.py
@@ -1,13 +1,30 @@
+import datetime
 import logging
 
 from django.core.handlers.wsgi import WSGIRequest
 from django.template.response import TemplateResponse
 
+from lando.main.models.jobs import JobStatus
+from lando.main.models.landing_job import LandingJob
 from lando.ui.views import LandoView
 
 logger = logging.getLogger(__name__)
 
 
 class IndexView(LandoView):
+    FINAL_JOBS_MAX_DAYS = 7
+
     def get(self, request: WSGIRequest) -> TemplateResponse:
-        return TemplateResponse(request=request, template="home.html")
+        context = {}
+        if request.user.is_authenticated:
+            context["pending_jobs"] = LandingJob.objects.filter(
+                requester_email=request.user.email, status__in=JobStatus.pending()
+            )
+            context["final_jobs"] = LandingJob.objects.filter(
+                requester_email=request.user.email,
+                status__in=JobStatus.final(),
+                updated_at__gt=datetime.datetime.now()
+                - datetime.timedelta(days=self.FINAL_JOBS_MAX_DAYS),
+            )
+
+        return TemplateResponse(request=request, template="home.html", context=context)
