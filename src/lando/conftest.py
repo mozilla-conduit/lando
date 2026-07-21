4f3c48375c6a44fcd2202008abb8da7f40d0ed6d
Magnolia Liu <magnolia.liu@mail.utoronto.ca>
2026-07-21 16:45:30 -0400

    wrap or statement

--- src/lando/conftest.py
+++ src/lando/conftest.py
@@ -1163,7 +1163,7 @@ def create_git_commit(
         name: str | None = None,
         content: str | None = None,
     ) -> str:
-        new_file = clone_path / name or str(uuid.uuid4())
+        new_file = clone_path / (name or str(uuid.uuid4()))
         new_file.write_text(content or request.node.name, encoding="utf-8")
 
         subprocess.run(["git", "add", new_file.name], cwd=str(clone_path), check=True)
