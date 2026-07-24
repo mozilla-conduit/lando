de53cd0f0f0cf1360f12dedc0c45737c4aba8137
Magnolia Liu <magnolia.liu@mail.utoronto.ca>
2026-07-24 09:39:48 -0400

    don't return string
    Co-authored-by: Olivier Mehani <omehani@mozilla.com>

--- src/lando/api/legacy/workers/automation_worker.py
+++ src/lando/api/legacy/workers/automation_worker.py
@@ -238,11 +238,12 @@ def get_reverted_pr(
     expected_repo = github_client.repo_name
 
     if pr_owner != expected_owner or pr_repo != expected_repo:
-        return (
+        logger.warning (
             f"Skipping commit {original_commit_hash} because PR URL in commit message "
             f"[{original_commit_message}] points to unexpected repo: {pr_owner}/{pr_repo}, "
             f"but automation worker expected PRs to be from {expected_owner}/{expected_repo}."
         )
+        return None
 
     try:
         pr_to_revert = github_client.build_pull_request(pr_number)
