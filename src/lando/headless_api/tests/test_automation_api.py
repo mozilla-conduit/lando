0e0ebcdec5544b8b2d10da38f4f80eb07aea99c4
Magnolia Liu <magnolia.liu@mail.utoronto.ca>
2026-08-07 15:46:18 -0400

    rename tests and add docstrings

--- src/lando/headless_api/tests/test_automation_api.py
+++ src/lando/headless_api/tests/test_automation_api.py
@@ -2232,7 +2232,7 @@ def make_pull_request(
 
 @mock.patch("lando.api.legacy.workers.automation_worker.GitHubAPIClient")
 @pytest.mark.django_db
-def test_automation_job_pipeline(
+def test_comment_on_reverted_pr_single_revert(
     github_api_client,
     client,
     git_repo_github_push_path,
@@ -2243,6 +2243,7 @@ def test_automation_job_pipeline(
     headless_user,
     create_git_commit,
 ):
+    """Test that a comment is added to a PR when it is reverted by an automation job."""
 
     user, token = headless_user
 
@@ -2317,7 +2318,7 @@ def test_automation_job_pipeline(
 
 @mock.patch("lando.api.legacy.workers.automation_worker.GitHubAPIClient")
 @pytest.mark.django_db
-def test_automation_job_pipeline_2_commits_reverted(
+def test_comment_on_reverted_prs_multiple_reverts_in_one_commit(
     github_api_client,
     client,
     git_repo_github_push_path,
@@ -2328,7 +2329,7 @@ def test_automation_job_pipeline_2_commits_reverted(
     headless_user,
     create_git_commit,
 ):
-
+    """Test that comments are added to multiple PRs when they are reverted by a single automation job."""
     user, token = headless_user
 
     repo = git_repo_github_push_path
@@ -2451,7 +2452,7 @@ def test_automation_job_pipeline_2_commits_reverted(
 
 @mock.patch("lando.api.legacy.workers.automation_worker.GitHubAPIClient")
 @pytest.mark.django_db
-def test_automation_job_pipeline_sandwiched_revert(
+def test_comment_on_reverted_pr_among_non_revert_commits(
     github_api_client,
     client,
     git_repo_github_push_path,
@@ -2462,7 +2463,7 @@ def test_automation_job_pipeline_sandwiched_revert(
     headless_user,
     create_git_commit,
 ):
-
+    """Test that a comment is added to a PR when it is reverted by an automation job, even if the revert commit is not the most recent commit."""
     user, token = headless_user
 
     repo = git_repo_github_push_path
