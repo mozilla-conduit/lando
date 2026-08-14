d874807845a93d8a107b2f9091825501669b636a
Olivier Mehani <omehani@mozilla.com>
2026-08-14 11:26:58 +1000

    Reapply "landing_checks: return all blocking errors at once (bug 2060110) r=sheehan" (bug 2063219)
    This reverts commit 3eea0e72491ee1a13852ae27dbd16e60cc0d3e2d.

--- src/lando/api/legacy/commit_message.py
+++ src/lando/api/legacy/commit_message.py
@@ -48,23 +48,22 @@ REVIEWERS_RE = re.compile(
 # Currently just MozReview-Commit-ID
 METADATA_RE = re.compile("^MozReview-Commit-ID: ")
 
-ACCEPTABLE_MESSAGE_FORMAT_RES = [
+BUG_RES = [
     re.compile(format, re.I)
     for format in [
         r"bug [0-9]+",
         r"no bug",
-        r"^(back(ing|ed)?\s+out|backout).*(\s+|\:)[0-9a-f]{12}",
-        r"^(revert(ed|ing)?).*(\s+|\:)[0-9a-f]{12}",
-        r"^add(ed|ing)? tag",
     ]
 ]
 INVALID_REVIEW_FLAG_RE = re.compile(r"[\s.;]r\?(?:\w|$)")
+
+ADD_TAG_RE = re.compile(r"^add(ed|ing)? tag", re.I)
 
 CHANGESET_KEYWORD = r"(?:\b(?:changeset|revision|change|cset|of)\b)"
 CHANGESETS_KEYWORD = r"(?:\b(?:changesets|revisions|changes|csets|of)\b)"
 SHORT_NODE = r"([0-9a-f]{12}\b)"
 SHORT_NODE_RE = re.compile(SHORT_NODE, re.I)
-BACKOUT_KEYWORD = r"^(?:backed out|backout|back out)\b"
+BACKOUT_KEYWORD = r"^(?:backed out|backout|back out|revert(?:ed|ing)?(?: to)?)\b"
 BACKOUT_KEYWORD_RE = re.compile(BACKOUT_KEYWORD, re.I)
 BACKOUT_SINGLE_RE = re.compile(
     BACKOUT_KEYWORD
@@ -89,8 +88,6 @@ BACKOUT_MULTI_ONELINE_RE = re.compile(
     + r")+)",
     re.I,
 )
-RE_SOURCE_REPO = re.compile(r"^Source-Repo: (https?:\/\/.*)$", re.MULTILINE)
-RE_SOURCE_REVISION = re.compile(r"^Source-Revision: (.*)$", re.MULTILINE)
 
 RE_DIFF = re.compile(r"[\n^]diff .*\n.*\n(\+\+\+|---).*\n", re.DOTALL)
 
@@ -103,6 +100,11 @@ def is_backout(commit_desc: str) -> bool:
     True here and None from parse_backouts().
     """
     return BACKOUT_KEYWORD_RE.match(commit_desc) is not None
+
+
+def is_tag(commit_desc: str) -> bool:
+    """Returns True if commit description indicates the changeset adds an (Hg) tag."""
+    return ADD_TAG_RE.match(commit_desc) is not None
 
 
 def parse_backouts(
