e9d96c007fd752f313bdabe13f95e78544108325
Olivier Mehani <omehani@mozilla.com>
2026-08-19 18:31:38 +1000

    repo: add SHIPPING flag (bug 2064629)

--- src/lando/main/models/repo.py
+++ src/lando/main/models/repo.py
@@ -35,6 +35,15 @@ DONTBUILD = (
         " new bug is close to none."
     ),
 )
+# SHIPPING flag (bug 2064629)
+SHIPPING = (
+    "SHIPPING",
+    (
+        "Should be used for changes intended to be shipped (uplift, chemspills, etc.),"
+        + " and for which results should be available as quickly as possible"
+        + " (and this needs the job and CI to be processed with higher priority)."
+    ),
+)
 
 
 def validate_path_in_repo_root(value: str):
