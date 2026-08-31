149c057ac71b7b738678d2c7fcfb5984eee0386e
Magnolia Liu <magnolia.liu@mail.utoronto.ca>
2026-08-31 10:21:52 -0400

    re-add setting status_code field after rebase

--- src/lando/utils/landing_checks.py
+++ src/lando/utils/landing_checks.py
@@ -698,26 +698,22 @@ class BugReferencesCheck(PatchCollectionCheck):
         # Check a single bug to determine which error to return.
         bug_id = invalid_bugs.pop()
         try:
-            status_code = get_status_code_for_bug(bug_id)
+            self.status_code = get_status_code_for_bug(bug_id)
         except requests.exceptions.RequestException as exc:
             return [BUG_REFERENCES_BMO_ERROR_TEMPLATE.format(error=str(exc))]
 
-        if status_code == 401:
-            return [
-                (
-                    f"Your commit message references bug {bug_id}, which is currently private. To avoid "
-                    "disclosing the nature of this bug publicly, please remove the affected bug ID "
-                    f"from the commit message. {BMO_SKIP_HINT}"
-                )
-            ]
+        if self.status_code == 401:
+            return (
+                f"Your commit message references bug {bug_id}, which is currently private. To avoid "
+                "disclosing the nature of this bug publicly, please remove the affected bug ID "
+                f"from the commit message. {BMO_SKIP_HINT}"
+            )
 
-        if status_code == 404:
-            return [
-                (
-                    f"Your commit message references bug {bug_id}, which does not exist. "
-                    f"Please check your commit message and try again. {BMO_SKIP_HINT}"
-                )
-            ]
+        if self.status_code == 404:
+            return (
+                f"Your commit message references bug {bug_id}, which does not exist. "
+                f"Please check your commit message and try again. {BMO_SKIP_HINT}"
+            )
 
         return [
             (
