c5337654d4ee689c1dd4e6651aa0c175c36bbe03
Olivier Mehani <omehani@mozilla.com>
2026-09-01 16:12:20 +1000

    fixup! HgSCM: delete all locks in startup maintenance (bug 2066955)

--- src/lando/main/tests/test_hg.py
+++ src/lando/main/tests/test_hg.py
@@ -597,8 +597,8 @@ def test_HgSCM__startup_maintenance(
     scm.startup_maintenance()
 
     # The locks got unconditionally deleted.
-    assert not wlock.exists(), "wlock should have been deleted by startup maintenance"
-    assert not lock.exists(), "lock should have been deleted by startup maintenance"
+    assert not wlock.is_symlink(), "wlock symlink should have been deleted by startup_maintenance"
+    assert not lock.is_symlink(), "lock symlink should have been deleted by startup_maintenance"
 
 
 def _trim_variable_patch_parts(patch: str):
