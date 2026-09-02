e6df4e47b04ceab2b11240f9d1ed93ce24d7e65d
Olivier Mehani <omehani@mozilla.com>
2026-09-02 17:15:33 +1000

    fixup! Reapply "landing_checks: return all blocking errors at once (bug 2060110) r=sheehan" (bug 2063219)

--- src/lando/utils/tests/test_landing_checks.py
+++ src/lando/utils/tests/test_landing_checks.py
@@ -601,7 +601,7 @@ def test_check_prevent_nspr_nss_combined():
         "Revision makes changes to restricted directories: vendored NSS directories: "
         "`security/nss/testfile.txt` vendored NSPR directories: `nsprpub/testfile.txt`."
     )
-    assert expected in prevent_nspr_nss_check.result(), (
+    assert [expected] == prevent_nspr_nss_check.result(), (
         "Check should disallow changes to both NSS and NSPR without proper commit message."
     )
 
