31ddf3a440b378566487fdf18606297e8e798804
Olivier Mehani <omehani@mozilla.com>
2026-09-02 17:10:50 +1000

    fixup! a3a103c60130e570ba585a95186667f996bf935d

--- src/lando/utils/landing_checks.py
+++ src/lando/utils/landing_checks.py
@@ -550,7 +550,7 @@ class CommitMessagesCheck(PatchCollectionCheck):
         # Backouts and Hg tags don't need bug numbers.
         if (
             not is_backout(firstline)
-            and not is_tag(firstline)
+            and not any(pattern.search(firstline) for pattern in BUG_RES)
             and not any(re.search(pattern, firstline) for pattern in BUG_RES)
         ):
             self.commit_message_issues.append(
