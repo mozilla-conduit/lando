f5dae8ec959ba2be61652e39bd2e1eff52f48f43
Olivier Mehani <omehani@mozilla.com>
2026-09-09 11:13:15 +1000

    Update src/lando/main/scm/hg.py

--- src/lando/main/scm/hg.py
+++ src/lando/main/scm/hg.py
@@ -813,7 +813,7 @@ class HgSCM(AbstractSCM):
             try:
                 self.run_hg(["strip", "--no-backup", "-r", "not public()"])
             except HgException as exc:
-                if exc.err == "abort: empty revision set\n":
+                if exc.err.strip() == "abort: empty revision set":
                     # This is a noop. No need to report this.
                     return
                 logger.exception(exc)
