6782ce298df858ea062a17e0989c152e74d4b93c
Olivier Mehani <omehani@mozilla.com>
2026-09-15 01:14:31 +0000

    github_helpers: split Lando-specific GitHub logic (bug 2057141) r=magnolia,sheehan,zeid
    This commit separates Lando- and Django-specific GitHub adapters from
    the project-agnostic GitHub logic. This mainly moves methods to new
    files, though some tidbits of logic are moved to the call-sites, with the goal
    to not import any project module from the util.github component.
    
    This does the same for tests, separating them to match the split, and
    moving GitHub fixtures to a dedicated conftest, which is explicitly
    re-imported.
    
    Pull request: https://github.com/mozilla-conduit/lando/pull/1345

--- src/lando/settings.py
+++ src/lando/settings.py
@@ -14,6 +14,7 @@ import os
 from pathlib import Path
 
 from lando.environments import Environment
+from lando.utils.github import GitHubSettings
 from lando.version import version
 
 DEBUG = os.getenv("DEBUG", "").lower() in ("true", "1")
@@ -337,6 +338,11 @@ GITHUB_APP_ID = os.getenv("GITHUB_APP_ID")
 GITHUB_APP_PRIVKEY = os.getenv("GITHUB_APP_PRIVKEY")
 
 HTTP_USER_AGENT = f"Lando/{version} ({ENVIRONMENT})"
+
+# Forward our own config to the GitHub utils'.
+GitHubSettings.GITHUB_APP_ID = GITHUB_APP_ID
+GitHubSettings.GITHUB_APP_PRIVKEY = GITHUB_APP_PRIVKEY
+GitHubSettings.HTTP_USER_AGENT = HTTP_USER_AGENT
 
 AUDITLOG_INCLUDE_ALL_MODELS = True
 AUDITLOG_EXCLUDE_TRACKING_MODELS = (
